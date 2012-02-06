6072a16862f2a84b7c6170ae4b71dc7bcab2bbe2
Ian McIntosh <ian@openanswers.org>
2012-02-05 21:31:55 -0800

    - cleanup

--- gui/git_committer_window.rb
+++ gui/git_committer_window.rb
@@ -1,28 +1,23 @@
 require 'glade_window'
 
-#require 'fileutils'
-
 class GitCommitterWindow < GladeWindow
 	def initialize(path)
 		super('git_committer_window')
 		@path = path
+		
+		# TODO: create treeview
+
+		add_changed_files
 	end
 
+	def add_changed_files
+		# TODO
+	end
+
+	#
+	# Callbacks
+	#
 	def on_delete_event
 		Gtk.main_quit		# quit app when main window is closed
-	end
-
-#	def on_play_button_clicked
-#	end
-
-#	def on_file_chosen
-#	end
-
-	def send_progress(progress)
-		# create OSC message
-		message = OSC::Message.new("Audio / Progress", 'f', progress)
-
-		# send it out
-		@socket.send(message.encode, 0, MESSAGE_BUS_IP, MESSAGE_BUS_PORT)		# 0 means calculate the length
 	end
 end
