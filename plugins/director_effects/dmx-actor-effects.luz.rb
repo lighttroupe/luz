16410bc8ec7155d1d503ec999a328e6c9c2a9600
Ian McIntosh <ian@openanswers.org>
2012-02-05 18:01:05 -0800

    - add pan/tilt control to DMX Actor Effects for moving-head DMX lights, and optional 'brightness' control for lights without RGB settings

--- plugins/director_effects/dmx-actor-effects.luz.rb
+++ plugins/director_effects/dmx-actor-effects.luz.rb
@@ -20,9 +20,24 @@ class DirectorEffectDMXActorEffects < DirectorEffect
 						$engine.user_object_try(light) {					# this ensures the plugin is usable and blames it for any exceptions from here on in
 							with_env(:child_index, index) {					# this can have an effect on the resulting color set
 								actor_effects.render_recursive {			# run actor's effects, and when they're done...
-									color = GL.GetColorArray						# ...read the final color and send it to the DMX plugin (respecting alpha!)
-									light.resolve_settings
-									light.red, light.green, light.blue = color[0]*color[3], color[1]*color[3], color[2]*color[3]		# NOTE: assumes DMX plugins have 'red' 'green' 'blue' methods
+									light.resolve_settings							# this is for settings that we're not manually setting values for below
+
+									# Set RGB
+									if light.respond_to? :red
+										color = current_color_array				# ...read the final color and send it to the DMX plugin (respecting alpha!)
+										light.red, light.green, light.blue = color[0]*color[3], color[1]*color[3], color[2]*color[3]
+									elsif light.respond_to? :brightness
+										color = current_color_array
+										light.brightness = ((color[0] + color[1] + color[2]) / 3.0) * color[3]
+									end
+
+									# Set pan and tilt
+									if light.respond_to? :pan
+										translate_x, translate_y, translate_z = current_xyz_translation
+										light.pan = (translate_x + 0.5).clamp(0.0, 1.0)
+										light.tilt = (translate_y + 0.5).clamp(0.0, 1.0)
+									end
+
 									light.tick													# let plugin do its work
 								}
 							}
