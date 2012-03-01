55ed4510594dd5594c8fcf1eaaecf3bc8c5dc854
Ian McIntosh <ian@openanswers.org>
2012-02-29 22:43:19 -0800

    - move webcam functionality out of plugin and into ruby component of C module

--- plugins/actor_effects/webcam.luz.rb
+++ plugins/actor_effects/webcam.luz.rb
@@ -5,18 +5,14 @@ class ActorEffectWebcam < ActorEffect
 	description ""
 
 	def after_load
-		require 'video/video4linux2'
+		require 'video/video4linux2.so'
+		require 'video/video4linux2.rb'
 		super
 	end
 
 	def render
 		$webcam ||= Video4Linux2::Camera.new
-		@image ||= Image.new
-
-		data = $webcam.data
-		@image.from_rgb8(data, $webcam.width, $webcam.height)
-
-		@image.using {
+		$webcam.with_frame(offset=0) {
 			yield
 		}
 	end
