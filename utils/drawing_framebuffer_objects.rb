9301b92bdd6e74c90bd7ba999b1add82fb3a3429
Ian McIntosh <ian@openanswers.org>
2012-03-07 19:22:32 -0800

    - add framebuffer size selection feature

--- utils/drawing_framebuffer_objects.rb
+++ utils/drawing_framebuffer_objects.rb
@@ -19,22 +19,29 @@
 require 'framebuffer_object'
 
 module DrawingFramebufferObjects
-	def get_offscreen_buffer
-		@fbo_list ||= []
+	DEFAULT_FRAMEBUFFER_SIZE = :large
+	FRAMEBUFFER_SIZES = {:large => {:width => 1024, :height => 768}, :medium => {:width => 512, :height => 384}, :small => {:width => 256, :height => 192}}
 
-		if @fbo_list.empty?
-			FramebufferObject.new
+	def get_offscreen_buffer(size = DEFAULT_FRAMEBUFFER_SIZE)
+		@fbo_to_size ||= {}			# so we know which stack to return it to
+		@fbo_hash ||= {}				# hash[size] => [fbo, fbo, ...]
+		@fbo_hash[size] ||= []
+
+		if @fbo_hash[size].empty?
+			fbo = FramebufferObject.new(FRAMEBUFFER_SIZES[size])
+			@fbo_to_size[fbo] = size		# record size of new framebuffer
+			fbo
 		else
-			@fbo_list.pop
+			@fbo_hash[size].pop
 		end
 	end
 
 	def return_offscreen_buffer(fbo)
-		@fbo_list << fbo
+		@fbo_hash[@fbo_to_size[fbo]] << fbo
 	end
 
-	def with_offscreen_buffer
-		fbo = get_offscreen_buffer
+	def with_offscreen_buffer(size = DEFAULT_FRAMEBUFFER_SIZE)
+		fbo = get_offscreen_buffer(size)
 		yield fbo
 		return_offscreen_buffer(fbo)
 	end
