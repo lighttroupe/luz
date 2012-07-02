79804983eb518c782ea4c4551c8b398286c64e5c
Ian McIntosh <ian@openanswers.org>
2012-07-01 17:18:56 -0700

    - working video file support

--- utils/video-file/ruby-ffmpeg.c
+++ utils/video-file/ruby-ffmpeg.c
@@ -41,25 +41,7 @@ static VALUE FFmpeg_File_data(VALUE self) {
 
 			// Did we get a video frame?
 			if(frame_finished > 0) {
-
-				int
-					width   = video_file->pCodecCtx->width,
-					height  = video_file->pCodecCtx->height,
-					width2  = ( width  + 1 ) / 2,
-					height2 = ( height + 1 ) / 2,
-					widtha  = ( width  + 7 ) & ~0x7,
-					width2a = ( width2 + 7 ) & ~0x7;
-
-					picture.data[0] = (uint8_t *)RSTRING_PTR(video_file->ruby_string_buffer);
-					picture.data[2] = (uint8_t *)RSTRING_PTR(video_file->ruby_string_buffer) + widtha * height;
-					picture.data[1] = (uint8_t *)picture.data[2] + width2a * height2;
-					picture.linesize[0] = widtha;
-					picture.linesize[1] = width2a;
-					picture.linesize[2] = width2a;
-
-				 sws_scale( video_file->sws_context, (video_file->pFrame->data), video_file->pFrame->linesize, 0,
-										 video_file->pCodecCtx->height, picture.data, picture.linesize );
-
+				sws_scale(video_file->sws_context, (video_file->pFrame->data), video_file->pFrame->linesize, 0, video_file->pCodecCtx->height, video_file->pFrameRGB->data, video_file->pFrameRGB->linesize);
 				return video_file->ruby_string_buffer;
 			}
 		}
@@ -72,19 +54,10 @@ static VALUE FFmpeg_File_free(VALUE self) {
 }
 
 static VALUE FFmpeg_File_new(VALUE klass, VALUE v_file_path) {
-	init_ffmpeg();
+	init_ffmpeg();		// lazy init
 	char* file_path = RSTRING_PTR(v_file_path);		// eg. "/dev/video0"
 
 	video_file_t* video_file = ALLOC_N(video_file_t, 1);
-
-/*
-	AVFormatContext *pFormatCtx;
-	AVCodecContext  *pCodecCtx;
-	AVCodec         *pCodec;
-	AVFrame         *pFrame; 
-	AVFrame         *pFrameRGB;
-	AVPacket        packet;
-*/
 
 	//
 	// Open video file
@@ -97,8 +70,9 @@ static VALUE FFmpeg_File_new(VALUE klass, VALUE v_file_path) {
 	if(av_find_stream_info(video_file->pFormatCtx) < 0)
 		return Qnil;
 
-	dump_format(video_file->pFormatCtx, 0, file_path, 0);
+	dump_format(video_file->pFormatCtx, 0, file_path, 0);		// console debug output
 
+	// Find video stream
 	int i;
 	for(i=0; i<(video_file->pFormatCtx->nb_streams); i++) {
 		if(video_file->pFormatCtx->streams[i]->codec->codec_type == CODEC_TYPE_VIDEO) {
@@ -106,10 +80,9 @@ static VALUE FFmpeg_File_new(VALUE klass, VALUE v_file_path) {
 			break;
 		}
 	}
-
 	video_file->pCodecCtx = video_file->pFormatCtx->streams[video_file->video_index]->codec;
 
-	// Find the decoder for the video stream
+	// Find decoder for video stream
 	video_file->pCodec = avcodec_find_decoder(video_file->pCodecCtx->codec_id);
 	if(video_file->pCodec == NULL) {
 		fprintf(stderr, "Unsupported codec!\n");
@@ -119,17 +92,19 @@ static VALUE FFmpeg_File_new(VALUE klass, VALUE v_file_path) {
 	if(avcodec_open(video_file->pCodecCtx, video_file->pCodec) < 0)
 		return Qnil;
 
-	video_file->sws_context = sws_getContext( video_file->pCodecCtx->width, video_file->pCodecCtx->height,
-																 video_file->pCodecCtx->pix_fmt,
-																 video_file->pCodecCtx->width, video_file->pCodecCtx->height,
-																 PIX_FMT_YUV420P, SWS_FAST_BILINEAR, 0, 0, 0 );
+	// Get scaling context (eg. YUV=>RGB)
+	video_file->sws_context = sws_getContext(video_file->pCodecCtx->width, video_file->pCodecCtx->height,	// input size
+																video_file->pCodecCtx->pix_fmt,
+																video_file->pCodecCtx->width, video_file->pCodecCtx->height,	// output size
+																//PIX_FMT_YUV420P, SWS_FAST_BILINEAR, 0, 0, 0 );
+																PIX_FMT_RGB24, SWS_FAST_BILINEAR, 0, 0, 0 );
 
 	video_file->pFrame = avcodec_alloc_frame();
 	video_file->pFrameRGB = avcodec_alloc_frame();
 
 	int buffer_size = avpicture_get_size(PIX_FMT_RGB24, video_file->pCodecCtx->width, video_file->pCodecCtx->height);
 
-// TODO: do we need to use av_malloc ... uint8_t* buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
+// TODO: doesn't seem to need to use av_malloc ... uint8_t* buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
 
 	char* temp_buffer = ALLOC_N(char, buffer_size);		// rb_str_new() sometimes segfaults with just ""
 	video_file->ruby_string_buffer = rb_str_new(temp_buffer, buffer_size);
@@ -141,9 +116,6 @@ static VALUE FFmpeg_File_new(VALUE klass, VALUE v_file_path) {
 	// "Note that pFrameRGB is an AVFrame, but AVFrame is a superset of AVPicture"
 	avpicture_fill((AVPicture *)(video_file->pFrameRGB), RSTRING_PTR(video_file->ruby_string_buffer), PIX_FMT_RGB24, video_file->pCodecCtx->width, video_file->pCodecCtx->height);
 
-	//
-	// ?
-	//
 	return Data_Wrap_Struct(vFileClass, 0, FFmpeg_File_free, video_file);
 }
 
