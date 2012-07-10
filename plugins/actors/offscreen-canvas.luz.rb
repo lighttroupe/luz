355ca6496629e671699d9c2d7f53c0f7884ff047
Ian McIntosh <ian@openanswers.org>
2012-07-10 01:32:16 -0700

    - fix flipbook functionality

--- plugins/actors/offscreen-canvas.luz.rb
+++ plugins/actors/offscreen-canvas.luz.rb
@@ -20,7 +20,7 @@ class ActorOffscreenCanvas < Actor
 	title				"Canvas"
 	description "A canvas upon which the Actor Render or Actor Pen plugins can draw."
 
-	setting 'pages', :integer, :range => 1..100, :default => 1..6
+	setting 'pages', :integer, :range => 1..1000, :default => 1..6
 	setting 'forward', :event
 	setting 'backward', :event
 	#setting 'previous_color', :color, :default => [1.0, 1.0, 1.0, 0.2]
@@ -28,10 +28,6 @@ class ActorOffscreenCanvas < Actor
 	hint 'The drawn image is persistent, unless erased by effects.'
 
 	FBO_USING_OPTIONS = {:clear => false}
-
-	def after_load
-		@fbos ||= Hash.new { |hash, key| hash[key] = create_fbo }
-	end
 
 	def render
 		current_fbo.with_image { unit_square }
@@ -46,15 +42,19 @@ class ActorOffscreenCanvas < Actor
 
 private
 
-	def create_fbo
-		FramebufferObject.new(:height => 1024, :width => 1024)
-	end
-
 	def current_fbo
-		@fbos[page_index]
+		fbos[page_index]
 	end
 
 	def page_index
 		(forward.count - backward.count) % pages
+	end
+
+	def fbos
+		@fbos ||= Hash.new { |hash, key| hash[key] = create_fbo }
+	end
+
+	def create_fbo
+		FramebufferObject.new(:height => 1024, :width => 1024)
 	end
 end
