39a27da89577225dc8af0d7bd49dd45e19f5427d
Ian McIntosh <ian@openanswers.org>
2012-08-28 20:39:45 -0700

    - easy_accessor should also make basic accessor

--- utils/easy_accessor.rb
+++ utils/easy_accessor.rb
@@ -1,6 +1,7 @@
 class Module
 	def easy_accessor(*signals)
 		signals.each { |signal|
+			module_eval( "def #{signal}() ; @#{signal} ; end", __FILE__, __LINE__ )
 			module_eval( "def #{signal}=(value) @#{signal} = value ; end", __FILE__, __LINE__ )
 			module_eval( "def set_#{signal}(value) @#{signal} = value ; self ; end", __FILE__, __LINE__ )
 		}
