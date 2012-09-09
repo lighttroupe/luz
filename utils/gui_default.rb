56e0ce34ad83b0f513d39a2de56f98a1bed59d1c
Ian McIntosh <ian@openanswers.org>
2012-09-09 14:24:55 -0700

    - remove test button, move text up to message bar area

--- utils/gui_default.rb
+++ utils/gui_default.rb
@@ -6,10 +6,7 @@ def create_default_gui
 	screen << (variable_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.23, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
 	screen << (events_list=GuiList.new($engine.project.events).set(:hidden => true, :scale_x => 0.12, :scale_y => 0.03, :offset_x => 0.4, :offset_y => 0.5, :spacing => 0.4))
 	screen << (event_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.40, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
-	screen << (text = BitmapFont.new.set(:string => 'Luz 2.0 has text support!!', :scale_x => 0.02, :scale_y => 0.04))
-
-	# Main menu
-	screen << (save_button = GuiButton.new.set(:scale_x => 0.1, :scale_y => 0.1, :offset_y => 0.2, :background_image => $engine.load_image('images/buttons/menu.png')))
+	screen << (text = BitmapFont.new.set(:string => 'Luz 2.0 has text support!!', :offset_x => -0.3, :offset_y => 0.5 - 0.03, :scale_x => 0.02, :scale_y => 0.04))
 
 	@cnt ||= 0
 	variable_button.on_clicked {
@@ -26,11 +23,6 @@ def create_default_gui
 		else
 			events_list.animate(:offset_y, 0.5, duration=0.25) { events_list.set_hidden(true) ; text.set_string(sprintf("no more events!")) }.animate(:opacity, 0.0, duration=0.2)
 		end
-	}
-
-	save_button.on_clicked {
-		#$engine.project.variables << $engine.project.variables.random.deep_clone
-		text.set_string(sprintf("clicked the button %d times", @cnt += 1))
 	}
 
 	return screen
