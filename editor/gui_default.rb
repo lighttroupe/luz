c5ec7360e3198bf3597e3de0165168ec9afa0dee
Ian McIntosh <ian@openanswers.org>
2012-09-12 05:59:12 -0700

    - list width

--- editor/gui_default.rb
+++ editor/gui_default.rb
@@ -12,9 +12,9 @@ class GuiDefault < GuiBox
 
 	def create_default_gui
 		#self << (actor_list=GuiList.new($engine.project.actors).set_scale(0.2).set_offset_x(-0.4).set_offset_y(0.4))
-		self << (@curves_list = GuiList.new($engine.project.curves).set(:hidden => true, :spacing_y => -1.4, :scale_x => 0.12, :scale_y => 0.03, :offset_x => -0.11, :offset_y => 0.5))
+		self << (@curves_list = GuiList.new($engine.project.curves).set(:hidden => true, :spacing_y => -1.4, :scale_x => 0.06, :scale_y => 0.03, :offset_x => -0.11, :offset_y => 0.5))
 		self << (@curve_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => -0.11, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
-		self << (@themes_list = GuiList.new($engine.project.themes).set(:hidden => true, :spacing_y => -1.4, :scale_x => 0.12, :scale_y => 0.03, :offset_x => 0.06, :offset_y => 0.5))
+		self << (@themes_list = GuiList.new($engine.project.themes).set(:hidden => true, :spacing_y => -1.4, :scale_x => 0.09, :scale_y => 0.045, :offset_x => 0.06, :offset_y => 0.5))
 		self << (@theme_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.06, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
 		self << (@variables_list = GuiList.new($engine.project.variables).set(:hidden => true, :spacing_y => -1.4, :scale_x => 0.12, :scale_y => 0.03, :offset_x => 0.23, :offset_y => 0.5))
 		self << (@variable_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.23, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
