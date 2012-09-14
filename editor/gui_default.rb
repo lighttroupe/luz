f7ebddcc6bb4700d3fc3a3fc4cff0c0c8e42038b
Ian McIntosh <ian@openanswers.org>
2012-09-14 03:51:24 -0700

    - layout

--- editor/gui_default.rb
+++ editor/gui_default.rb
@@ -12,7 +12,7 @@ class GuiDefault < GuiBox
 
 	def create_default_gui
 		#self << (actor_list=GuiList.new($engine.project.actors).set_scale(0.2).set_offset_x(-0.4).set_offset_y(0.4))
-		self << (@curves_list = GuiList.new($engine.project.curves).set(:hidden => true, :spacing_y => -1.2, :scale_x => 0.09, :scale_y => 0.045, :offset_x => -0.11, :offset_y => 0.5))
+		self << (@curves_list = GuiList.new($engine.project.curves).set(:hidden => true, :spacing_y => -1.2, :scale_x => 0.07, :scale_y => 0.04, :offset_x => -0.11, :offset_y => 0.5))
 		self << (@curve_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => -0.11, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
 		self << (@themes_list = GuiList.new($engine.project.themes).set(:hidden => true, :spacing_y => -1.2, :scale_x => 0.09, :scale_y => 0.045, :offset_x => 0.06, :offset_y => 0.5))
 		self << (@theme_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.06, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
