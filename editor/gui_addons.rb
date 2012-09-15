fa62441ec80d79c0ce0053880faf9240ff33ad30
Ian McIntosh <ian@openanswers.org>
2012-09-14 23:26:08 -0700

    - add EventInput#gui_render!

--- editor/gui_addons.rb
+++ editor/gui_addons.rb
@@ -254,3 +254,25 @@ class Event
 	end
 end
 
+class EventInput
+	GUI_COLOR_ON = [1.0,1.0,0.0,1.0]
+	GUI_COLOR_OFF = [1.0,1.0,0.0,0.1]
+
+	def gui_render!
+		render_selection if pointer_hovering?
+
+		# Status Indicator
+		with_translation(-0.5 + 0.1, 0.0) {
+			with_scale(0.1, 0.35) {
+				with_color(now? ? GUI_COLOR_ON : GUI_COLOR_OFF) {
+					unit_square
+				}
+			}
+		}
+
+		with_translation(0.17, 0.05) {
+			# Label
+			gui_render_label
+		}
+	end
+end
