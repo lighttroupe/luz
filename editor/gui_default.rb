c2b053eba9bda5d2c01302ef762f1f7b40fb2d3b
Ian McIntosh <ian@openanswers.org>
2012-09-14 18:41:10 -0700

    - test/hack at stepping the curves grid to the left

--- editor/gui_default.rb
+++ editor/gui_default.rb
@@ -14,7 +14,7 @@ class GuiDefault < GuiBox
 
 	def create_default_gui
 		#self << (actor_list=GuiList.new($engine.project.actors).set_scale(0.2).set_offset_x(-0.4).set_offset_y(0.4))
-		self << (@curves_list = GuiGrid.new($engine.project.curves).set(:scale_x => 0.07, :scale_y => 0.04, :offset_x => -0.11, :offset_y => 0.5, :hidden => true, :spacing_x => 1.2, :spacing_y => -1.2))
+		self << (@curves_list = GuiGrid.new($engine.project.curves).set(:scale_x => 0.07, :scale_y => 0.04, :offset_x => -0.11 + -1.2*0.07, :offset_y => 0.5, :hidden => true, :spacing_x => 1.2, :spacing_y => -1.2))
 		self << (@curve_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => -0.11, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
 		self << (@themes_list = GuiGrid.new($engine.project.themes).set(:scale_x => 0.09, :scale_y => 0.045, :offset_x => 0.06, :offset_y => 0.5, :hidden => true, :spacing_x => 1.2, :spacing_y => -1.2))
 		self << (@theme_button = GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.08, :offset_x => 0.06, :offset_y => 0.50 - 0.04, :background_image => $engine.load_image('images/buttons/menu.png')))
