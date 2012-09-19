5bef281286fef14cf278ae4ee90087e32137b171
Ian McIntosh <ian@openanswers.org>
2012-09-19 15:25:47 -0700

    - code reorder

--- editor/gui_list.rb
+++ editor/gui_list.rb
@@ -2,21 +2,19 @@ class GuiList < GuiBox
 	easy_accessor :spacing_x, :spacing_y, :item_aspect_ratio
 	easy_accessor :scroll
 
-=begin
-	def with_aspect_ratio_fix_y
-		width = $env[:gui_scale_x]
-		height = $env[:gui_scale_y]
-		with_scale(1.0, width/height) {		# multiply width as necessary to maintain a ratio of 1x2   TODO: dynamic?
-			with_env(:gui_scale_y, $env[:gui_scale_y] * width/height) {
-				yield
-			}
-		}
+	def gui_render!
+		return if hidden?
+		#with_positioning { with_color([0.5,1.0,0.0,0.5]) { unit_square } }
+		each_with_positioning { |gui_object| gui_object.gui_render! }
 	end
-=end
+
+	def hit_test_render!
+		return if hidden?
+		each_with_positioning { |gui_object| gui_object.hit_test_render! }
+	end
 
 	def each_with_positioning
 		@scroll ||= 0.0
-		# @scroll += 0.01		# scrolling test
 
 		with_positioning {
 			if spacing_y && spacing_y != 0.0
@@ -77,15 +75,4 @@ class GuiList < GuiBox
 		}
 	end
 
-	def gui_render!
-		return if hidden?
-		#with_positioning { with_color([0.5,1.0,0.0,0.5]) { unit_square } }
-		each_with_positioning { |gui_object| gui_object.gui_render! }
-		
-	end
-
-	def hit_test_render!
-		return if hidden?
-		each_with_positioning { |gui_object| gui_object.hit_test_render! }
-	end
 end
