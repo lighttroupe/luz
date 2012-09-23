8b39077ffa1c60f0466a930353738e786312e180
Ian McIntosh <ian@openanswers.org>
2012-09-22 17:28:45 -0700

    - draw_line helper

--- utils/drawing_lines.rb
+++ utils/drawing_lines.rb
@@ -25,4 +25,11 @@ module DrawingLines
 			$line_width_stack.pop
 		GL.LineWidth($line_width_stack.last || 1.0)
 	end
+
+	def draw_line(x1,y1,x2,y2)
+		GL.Begin(GL::LINES)
+			GL.Vertex(x1,y1)
+			GL.Vertex(x2,y2)
+		GL.End
+	end
 end
