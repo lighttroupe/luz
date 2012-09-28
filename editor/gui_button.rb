105dd6ecb9e9627bd9dc69adb4223d0c427ce249
Ian McIntosh <ian@openanswers.org>
2012-09-28 04:29:16 -0700

    - GuiButton adds hotkey support

--- editor/gui_button.rb
+++ editor/gui_button.rb
@@ -1,6 +1,6 @@
 class GuiButton < GuiObject
 	callback :clicked
-	easy_accessor :background_image
+	easy_accessor :background_image, :hotkey
 
 	def click(pointer)
 		clicked_notify(pointer)
@@ -9,6 +9,11 @@ class GuiButton < GuiObject
 	BUTTON_COLOR = [1.0,1.0,1.0,1.0]
 	BUTTON_HOVER_COLOR = [1.0,0.5,0.5]
 	BUTTON_CLICK_COLOR = [0.5,1.0,0.5]
+
+	def gui_tick!
+		super
+		clicked_notify(nil) if hotkey && $engine.button_pressed_this_frame?(hotkey)
+	end
 
 	def gui_color
 		(pointer_clicking?) ? BUTTON_CLICK_COLOR : ((pointer_hovering?) ? BUTTON_HOVER_COLOR : nil)
