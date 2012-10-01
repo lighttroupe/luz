2b092aefa3dde1e43e8da100d16e83b559e043de
Ian McIntosh <ian@openanswers.org>
2012-09-30 22:57:42 -0700

    - clamp return value in VariableInputButtonToggle

--- plugins/variable_inputs/button-toggle.luz.rb
+++ plugins/variable_inputs/button-toggle.luz.rb
@@ -27,10 +27,10 @@ class VariableInputButtonToggle < VariableInput
 	def value
 		if $engine.button_press_count(button).is_odd?
 			return 1.0 if on_time.instant?
-			return (last_value + ($env[:frame_time_delta] / on_time.to_seconds))
+			return (last_value + ($env[:frame_time_delta] / on_time.to_seconds)).clamp(0.0, 1.0)
 		else
 			return 0.0 if off_time.instant?
-			return (last_value - ($env[:frame_time_delta] / off_time.to_seconds))
+			return (last_value - ($env[:frame_time_delta] / off_time.to_seconds)).clamp(0.0, 1.0)
 		end
 	end
 end
