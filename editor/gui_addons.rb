ed5969fd549020db74fe692c83b564d985506f7c
Ian McIntosh <ian@openanswers.org>
2012-10-01 16:00:06 -0700

    - UserObjectSettingCurve layout

--- editor/gui_addons.rb
+++ editor/gui_addons.rb
@@ -93,7 +93,7 @@ class UserObjectSettingCurve
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiCurve.new(self, :curve)
+		box << GuiCurve.new(self, :curve).set(:scale_x => 0.15, :scale_y => 0.4, :float => :left, :offset_x => 0.04, :offset_y => 0.14)
 		box
 	end
 end
