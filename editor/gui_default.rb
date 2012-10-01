451781f09905f5c4c8000fd8289eb3bbfe895966
Ian McIntosh <ian@openanswers.org>
2012-09-30 22:12:17 -0700

    - preferences window background color

--- editor/gui_default.rb
+++ editor/gui_default.rb
@@ -247,7 +247,7 @@ class GuiPreferencesBox < GuiBox
 	end
 
 	def build
-		self << GuiObject.new.set(:color => [0.7, 1.0, 0.7, 0.9])
+		self << GuiObject.new.set(:color => [0.2, 0.5, 0.2, 0.9])
 		self << (@fps_label=BitmapFont.new.set_string("Frames Per Second").set(:scale_x => 0.05, :scale_y => 0.06, :offset_x => -0.42, :offset_y => 0.45))
 		self << GuiInteger.new($application, :frames_per_second, 20, 70).set(:offset_x => 0.4, :offset_y => 0.45, :scale_x => 0.2, :scale_y => 0.1)
 		self
