de23c67be40cab0b8474d01d55f03c4563205a3d
Ian McIntosh <ian@openanswers.org>
2012-10-11 23:50:32 -0700

    - ScreenCapture plugin refactoring

--- plugins/project_effects/screen-capture.luz.rb
+++ plugins/project_effects/screen-capture.luz.rb
@@ -26,38 +26,51 @@ class ProjectEffectScreenCapture < ProjectEffect
 
 	RAW_IMAGE_ENCODER_NAME = 'convert'
 
+	def get_framebuffer_rgb8
+		capture_start_time = Time.now
+		pixels = $application.get_framebuffer_rgb
+		puts "framebuffer capture took #{Time.now-capture_start_time} seconds"
+		pixels
+	end
+
+	def generate_output_filepath
+		time = Time.now
+		output_filename = sprintf("luz-screenshot-%04d_%02d_%02d-%02d_%02d_%02d_%04d-%06d.png", time.year, time.month, time.day, time.hour, time.min, time.sec, time.usec/100, $env[:frame_number]) # time for good file system sorting, frame number to further promote uniqueness
+		output_filepath = File.join($engine.project.file_path, output_filename)
+	end
+
+	def save_pixels_to_path(pixels, output_filepath)
+		# Check for presence of raw->png converter app NOTE: only does this once
+		@raw_image_encoder_present = !(open("|which #{RAW_IMAGE_ENCODER_NAME}").read.empty?) if @raw_image_encoder_present.nil?
+
+		if @raw_image_encoder_present
+			raw_filepath = output_filepath.sub(/\.png$/, '.raw')
+			File.open(raw_filepath, 'w') { |f| f.write(pixels) }
+
+			# Run 'convert' in another process
+			# Use 'nice' at maximum niceness to help prevent disruption - TODO: run all conversions at shutdown time?
+			cmd = "nice -n 19 convert -flip -depth 8 -size #{$application.width}x#{$application.height} rgb:\"#{raw_filepath}\" \"#{output_filepath}\" ; rm \"#{raw_filepath}\""
+			puts "executing: #{cmd}"
+			open("|#{cmd}")
+		else
+			puts "compressing screen capture in-process (HINT: install the ImageMagick 'convert' application for faster screenshots)" unless @warned
+			@warned = true
+
+			image = Magick::Image.new($application.width, $application.height)
+			image.import_pixels(0, 0, $application.width, $application.height, "RGB", pixels, Magick::CharPixel)
+			image.flip!			# data comes at us upside down
+			image.write(output_filepath)
+		end
+	end
+
 	def tick
 		if (event.now? and $gui.nil?)	# NOTE: only operates in performer
-			time = Time.now
-			output_filename = sprintf("luz-screenshot-%04d_%02d_%02d-%02d_%02d_%02d_%04d-%06d.png", time.year, time.month, time.day, time.hour, time.min, time.sec, time.usec/100, $env[:frame_number]) # time for good file system sorting, frame number to further promote uniqueness
-			output_filepath = File.join($engine.project.file_path, output_filename)
+			output_filepath = generate_output_filepath
 
 			# Get pixels and write them to temporary file
-			capture_start_time = Time.now
-			return if (pixels = $application.get_framebuffer_rgb).nil?		# not necessarily supported (eg. in editor)
-			puts "framebuffer capture took #{Time.now-capture_start_time} seconds"
+			return if (pixels = get_framebuffer_rgb8).nil?
 
-			# Check for presence of raw->png converter app NOTE: only does this once
-			@raw_image_encoder_present = !(open("|which #{RAW_IMAGE_ENCODER_NAME}").read.empty?) if @raw_image_encoder_present.nil?
-
-			if @raw_image_encoder_present
-				raw_filepath = output_filepath.sub(/\.png$/, '.raw')
-				File.open(raw_filepath, 'w') { |f| f.write(pixels) }
-
-				# Run 'convert' in another process
-				# Use 'nice' at maximum niceness to help prevent disruption - TODO: run all conversions at shutdown time?
-				cmd = "nice -n 19 convert -flip -depth 8 -size #{$application.width}x#{$application.height} rgb:\"#{raw_filepath}\" \"#{output_filepath}\" ; rm \"#{raw_filepath}\""
-				puts "executing: #{cmd}"
-				open("|#{cmd}")
-			else
-				puts "compressing screen capture in-process (HINT: install the ImageMagick 'convert' application for faster screenshots)" unless @warned
-				@warned = true
-
-				image = Magick::Image.new($application.width, $application.height)
-				image.import_pixels(0, 0, $application.width, $application.height, "RGB", pixels, Magick::CharPixel)
-				image.flip!			# data comes at us upside down
-				image.write(output_filepath)
-			end
+			save_pixels_to_path(pixels, output_filepath)
 		end
 	end
 end
