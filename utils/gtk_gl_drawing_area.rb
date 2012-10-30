4239d6b5fe90c9836211aa0f071015e3d0d78d7f
Ian McIntosh <ian@openanswers.org>
2012-10-29 22:31:44 -0700

    - ruby 1.9 in ubuntu 12.04 wants a require 'opengl' here

--- utils/gtk_gl_drawing_area.rb
+++ utils/gtk_gl_drawing_area.rb
@@ -16,6 +16,8 @@
  #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  ###############################################################################
 
+require 'opengl'
+
 class GtkGLDrawingArea < Gtk::DrawingArea
 	@@shared_gl_context = nil
 
