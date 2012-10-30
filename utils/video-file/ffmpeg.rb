c85767327c8c14d7114d7c30651751e92c6e7d70
Ian McIntosh <ian@openanswers.org>
2012-10-29 22:30:31 -0700

    - ffmpeg.rb cleanup

--- utils/video-file/ffmpeg.rb
+++ utils/video-file/ffmpeg.rb
@@ -11,8 +11,6 @@ rescue LoadError => e
 end
 
 module FFmpeg
-	MAX_FRAME_CACHE_SIZE = 50
-
 	class File
 		attr_reader :frame_index
 		def with_frame(index=0)
@@ -21,21 +19,24 @@ module FFmpeg
 			# loop index around on both sides
 			index = index % self.frame_count if self.frame_count > 0		# sometimes it's 0?!
 
-			#
-			# Can we satisfy this from cache?
-			#
 			if(index == @last_frame_index || @last_frame_load == $env[:frame_number])
 				#puts 'not moving forward'
 			else
 				# loop around?		TODO: this assumes video is moving forward
 				if(index == 0 && @last_frame_index && @last_frame_index != 0)
 					#puts 'seeking to 0'
-					self.seek_to_frame(0)
+					if self.seek_to_frame(0)
+						#puts 'video-file: seeked to 0'
+					else
+						puts 'video-file: seek failed'
+					end
 				end
 
 				if(new_data = self.read_next_frame)
-					#puts "#{index} got frame"
+					#puts "got frame #{index}"
 					@image.from_rgb8(new_data, self.width, self.height)
+				else
+					puts "video-file: read_next_frame failed"
 				end
 
 				@last_frame_index = index
@@ -46,28 +47,5 @@ module FFmpeg
 				yield
 			}
 		end
-
-=begin
-		def read_frame_index_into_image(index, image)
-			if index != @frame_index
-				#puts "#{index} seeking to"
-				unless self.seek_to_frame(index)
-					#puts "SEEKFAILED ==============================="
-				end
-				@frame_index = index
-			end
-
-			# Decode Frame
-			if(new_data = self.read_next_frame)
-				#puts "#{index} got frame"
-				image.from_rgb8(new_data, self.width, self.height)
-				@frame_index += 1
-			else
-				# Loop (TODO: this shouldn't be hit since we're correcting frame index above?)
-				self.seek_to_frame(0)
-				@frame_index = 0
-			end
-		end
-=end
 	end
 end
