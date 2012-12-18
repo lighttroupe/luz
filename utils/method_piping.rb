33559f945d8420ecdeb3230171ddec39fe19fb91
Ian McIntosh <ian@openanswers.org>
2012-12-18 02:38:28 -0800

    - method piping should support an array of methods

--- utils/method_piping.rb
+++ utils/method_piping.rb
@@ -1,14 +1,16 @@
 class Module
 	# Creates methods in a class that sends all calls of method 'signal' to
 	# calls to a method of the given target instance variable
-	def pipe(signal, target, options={})
-		options = {:method => signal}.merge(options)
-		if options[:args]
-			module_eval( "def #{signal}(&proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', #{options[:args]}, &proc) ; end", "__(METHOD PIPING)__", 1)
-		elsif options[:no_args]
-			module_eval( "def #{signal}(&proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', &proc) ; end", "__(METHOD PIPING)__", 1)
-		else
-			module_eval( "def #{signal}(*args, &proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', *args, &proc) ; end", "__(METHOD PIPING)__", 1)
-		end
+	def pipe(signal, target, original_options={})
+		(signal.respond_to?(:each) ? signal : [signal]).each { |signal|
+			options = {:method => signal}.merge(original_options)
+			if options[:args]
+				module_eval( "def #{signal}(&proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', #{options[:args]}, &proc) ; end", "__(METHOD PIPING)__", 1)
+			elsif options[:no_args]
+				module_eval( "def #{signal}(&proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', &proc) ; end", "__(METHOD PIPING)__", 1)
+			else
+				module_eval( "def #{signal}(*args, &proc) instance_variable_get('@' + '#{target.to_s}').send('#{options[:method]}', *args, &proc) ; end", "__(METHOD PIPING)__", 1)
+			end
+		}
 	end
 end
