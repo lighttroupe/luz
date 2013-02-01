40c2e963e5ca06ec4235e71b05ffa81c0e92ef99
Ian McIntosh <ian@openanswers.org>
2013-02-01 09:57:24 -0800

    - beat monitor tweaks

--- gui-ruby/gui_beat_monitor.rb
+++ gui-ruby/gui_beat_monitor.rb
@@ -11,8 +11,8 @@ class GuiBeatLight < GuiObject
 
 	def gui_render!
 		with_color(on? ? BEAT_ON_COLOR : BEAT_OFF_COLOR) {
-			with_scale(on? ? ($env[:beat_scale].scale(1.5, 1.0)) : 1.0) {
-				with_multiplied_alpha(on? ? ($env[:beat_scale].scale(1.0, 0.5)) : 0.25) {
+			with_scale(on? ? ($env[:beat_scale].scale(1.1, 1.0)) : 1.0) {
+				with_multiplied_alpha(on? ? ($env[:beat_scale].scale(1.0, 0.1)) : 0.0) {
 					unit_square
 				}
 			}
