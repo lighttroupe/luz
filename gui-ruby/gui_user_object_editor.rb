6eb27697a9bbd9a11922df6a177613ba934c13db
Ian McIntosh <ian@openanswers.org>
2013-02-05 21:40:40 -0800

    - comments

--- gui-ruby/gui_user_object_editor.rb
+++ gui-ruby/gui_user_object_editor.rb
@@ -9,13 +9,19 @@ class GuiUserObjectEditor < GuiBox
 	end
 
 	def create!
-		# background
+		#
+		# Background
+		#
 		self << (@background=GuiObject.new.set(:color => [0,0,0,1.0]))
 
-		# content
-		self << @user_object.gui_build_editor		# find gui_build_editor implementations for everything in gui_addons.rb
+		#
+		# Let object build its own content (eg. lists)
+		#
+		self << @user_object.gui_build_editor		# find gui_build_editor implementations in gui-ruby/addons
 
-		# label
+		#
+		# Title
+		#
 		if @user_object.has_settings_list?
 			self << (@title_button=GuiButton.new.set(:scale_x => 0.5, :offset_x => -0.25, :scale_y => 0.10, :offset_y => 0.5 - 0.06, :color => [0,0,0,1]))		#.set(:background_image => $engine.load_image('images/buttons/menu.png'))
 
@@ -30,6 +36,9 @@ class GuiUserObjectEditor < GuiBox
 			animate({:opacity => 0.0, :offset_y => offset_y - 0.1, :scale_x => scale_x * 1.1}, duration=0.2) { set_hidden(true) }
 		}
 
+		#
+		# Add button
+		#
 		self << (@add_child_button=GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.15, :offset_x => -0.54, :offset_y => -0.5 + 0.15 + 0.18, :background_image => $engine.load_image('images/buttons/add.png')))
 		@add_child_button.on_clicked { |pointer|
 			window = build_add_child_window_for(@user_object, pointer)
@@ -43,6 +52,9 @@ class GuiUserObjectEditor < GuiBox
 			self << window
 		}
 
+		#
+		# Remove button
+		#
 		self << (@remove_child_button=GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.15, :offset_x => -0.54, :offset_y => -0.5 + 0.15, :background_image => $engine.load_image('images/buttons/remove.png')))
 		@remove_child_button.on_clicked { |pointer|
 			remove_selected
