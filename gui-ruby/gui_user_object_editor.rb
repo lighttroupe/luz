463f0310bcb508758071040660958c1d67b228ae
Ian McIntosh <ian@openanswers.org>
2013-02-07 20:50:49 -0800

    - add down button and use it for UserObjectEditor

--- gui-ruby/gui_user_object_editor.rb
+++ gui-ruby/gui_user_object_editor.rb
@@ -33,9 +33,9 @@ class GuiUserObjectEditor < GuiBox
 		end
 		self << (@title_text=BitmapFont.new.set_string(@user_object.title).set(:scale_x => 0.5, :float => :left, :scale_y => 0.10, :offset_x => 0.0, :offset_y => 0.5 - 0.06))		#.set(:background_image => $engine.load_image('images/buttons/menu.png'))
 
-		self << (@close_button=GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.15, :offset_x => 0.5, :offset_y => 0.5, :background_image => $engine.load_image('images/buttons/close.png')))
+		self << (@close_button=GuiButton.new.set(:scale_x => 0.08, :scale_y => 0.15, :offset_x => 0.5, :offset_y => 0.5, :background_image => $engine.load_image('images/buttons/down.png')))
 		@close_button.on_clicked {
-			animate({:opacity => 0.0, :offset_y => offset_y - 0.1, :scale_x => scale_x * 1.1}, duration=0.2) { set_hidden(true) }
+			animate({:opacity => 0.0, :offset_y => offset_y - 0.2, :scale_x => scale_x * 1.1}, duration=0.2) { set_hidden(true) }
 		}
 
 		#
