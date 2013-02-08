f13b83c40f58c9a8c1a197a1fb2727d3f4b7d213
Ian McIntosh <ian@openanswers.org>
2013-02-08 01:49:53 -0800

    - move UOEditor buttons under effects list

--- gui-ruby/addons/user_object.rb
+++ gui-ruby/addons/user_object.rb
@@ -11,27 +11,67 @@ class UserObject
 
 	SHAKE_DISTANCE = 0.010
 
-	#
-	# API
-	#
-	attr_reader :gui_effects_list, :gui_settings_list
 	def gui_build_editor
 		if respond_to? :effects
 			box = GuiBox.new
 
-			# Two-lists side by side
-			@gui_effects_list = GuiListWithControls.new(effects).set({:spacing_y => -0.8, :scale_x => 0.29, :offset_x => -0.35, :scale_y => 0.87, :offset_y => -0.06, :item_aspect_ratio => 3.0})
+			# Effects list
+			@gui_effects_list = GuiListWithControls.new(effects).set({:spacing_y => -0.8, :scale_x => 0.3, :scale_y => 0.75, :offset_x => -0.35, :offset_y => 0.0, :item_aspect_ratio => 4.5})
 			box << @gui_effects_list
 
-			@gui_settings_list = GuiList.new.set({:spacing_y => -1.0, :scale_x => 0.69, :offset_x => 0.15, :scale_y => 0.87, :offset_y => -0.06, :item_aspect_ratio => 4.0})
-			box << @gui_settings_list
+			# Add Button
+			@add_child_button = GuiButton.new.set(:scale_x => 0.07, :scale_y => 0.11, :offset_x => -0.46, :offset_y => -0.5 + 0.07, :background_image => $engine.load_image('images/buttons/add.png'))
+			box << @add_child_button
+			@add_child_button.on_clicked { |pointer|
+				window = build_add_child_window_for_pointer(pointer)
+				window.on_add { |new_object|
+					@gui_effects_list.add_after_selection(new_object)
+					@gui_effects_list.set_selection(new_object)
+					@gui_effects_list.scroll_to_selection!
 
+					gui_fill_settings_list(new_object)
+				}
+				$gui << window
+			}
+
+			# Clone button
+			box << (@clone_button=GuiButton.new.set(:opacity => 0.5, :scale_x => 0.07, :scale_y => 0.11, :offset_x => -0.38, :offset_y => -0.5 + 0.07, :background_image => $engine.load_image('images/buttons/clone.png')))
+			@clone_button.on_clicked { |pointer|
+				clone_selected
+			}
+
+			# Remove button
+			box << (@remove_child_button=GuiButton.new.set(:scale_x => 0.07, :scale_y => 0.11, :offset_x => -0.30, :offset_y => -0.5 + 0.07, :background_image => $engine.load_image('images/buttons/remove.png')))
+			@remove_child_button.on_clicked { |pointer|
+				remove_selected
+			}
+
+			# Settings list
+			@gui_settings_list = GuiList.new.set({:spacing_y => -1.0, :scale_x => 0.59, :offset_x => 0.15, :scale_y => 0.87, :offset_y => -0.06, :item_aspect_ratio => 5.0})
+			box << @gui_settings_list
 			gui_fill_settings_list(self)		# show this object's settings
 
 			box
 		else
 			GuiObject.new		# nothing
 		end
+	end
+
+	def build_add_child_window_for_pointer(pointer)
+		window = GuiAddWindow.new(self)
+		window.set({:offset_x => 0.0, :offset_y => -0.65, :opacity => 0.0, :scale_x => 0.5, :scale_y => 0.4, :hidden => false})
+		window.animate({:offset_x => 0.0, :offset_y => -0.3, :scale_x => 0.5, :scale_y => 0.4, :opacity => 1.0}, duration=0.2)
+		window
+	end
+
+	def remove_selected
+		@gui_effects_list.selection.each { |object|
+			effects.delete(object)
+		}
+	end
+
+	def clone_selected
+		$gui.positive_message 'Clone...not implemented.'
 	end
 
 	def gui_fill_settings_list(user_object)
