a963fc0b62cae7a5bdcbf0906477d3da5d9e7e2d
Ian McIntosh <ian@openanswers.org>
2013-02-11 05:05:17 -0800

    - variable render-to-list makeover

--- gui-ruby/addons/variable.rb
+++ gui-ruby/addons/variable.rb
@@ -5,26 +5,46 @@ class Variable
 	def gui_render!
 		gui_render_background
 
-		# Value as Bar
 		value = do_value
+
+		render_bar_value_with_cache(value)
+		render_text_value_with_cache(value)
+
+		# Label
+		gui_render_label
+	end
+
+	def render_bar_value_with_cache(value)
+		cache_key = (1000 * value).to_i
+		@@value_list_cache ||= Hash.new
+		@@value_list_cache[cache_key] ||= GL.RenderToList { render_bar(value) }
+		GL.CallList(@@value_list_cache[cache_key])
+	end
+
+	def render_text_value_with_cache(value)
+		cache_key = (100 * value).to_i
+		@@text_list_cache ||= Hash.new
+		@@text_list_cache[cache_key] ||= GL.RenderToList { render_text(cache_key) }
+		GL.CallList(@@text_list_cache[cache_key])
+	end
+
+	def render_text(value)
+		@value_label ||= BitmapFont.new.set(:scale_x => 0.35, :scale_y => 0.35)
+		with_translation(0.45, 0.25) {
+			@value_label.set_string("#{value}%")
+			@value_label.gui_render!
+		}
+	end
+
+	def render_bar(value)
 		if value > 0.0
 			with_translation(-0.5 + value/2.0, 0.0) {
 				with_scale_unsafe(value, 1.0) {
-					with_color(GUI_COLOR) {
+					with_color_listsafe(GUI_COLOR) {
 						unit_square
 					}
 				}
 			}
 		end
-
-		# Value as Number
-		with_translation(0.45, 0.25) {
-			@value_label ||= BitmapFont.new.set(:scale_x => 0.35, :scale_y => 0.35)
-			@value_label.set_string((value * 100).to_i.to_s + '%')
-			@value_label.gui_render!
-		}
-
-		# Label
-		gui_render_label
 	end
 end
