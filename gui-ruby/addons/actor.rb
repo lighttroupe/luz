33346fda954eea267cd4c0bb90b8c4192f81c8ff
Ian McIntosh <ian@openanswers.org>
2013-02-26 00:07:05 -0800

    - move actor imaging code

--- gui-ruby/addons/actor.rb
+++ gui-ruby/addons/actor.rb
@@ -18,6 +18,37 @@ class Actor
 	end
 
 	def update_offscreen_buffer?
+		@update_offscreen_buffer_once ||= 5
+		return true if (@update_offscreen_buffer_once > 0) and (@update_offscreen_buffer_once -= 1)
 		pointer_hovering?
+	end
+
+	#
+	# ...
+	#
+	def init_offscreen_buffer
+		return if @offscreen_buffer
+		@offscreen_buffer = get_offscreen_buffer(framebuffer_image_size)
+		update_offscreen_buffer!
+	end
+
+	def framebuffer_image_size
+		:medium		# see drawing_framebuffer_objects.rb
+	end
+
+	def with_image
+		init_offscreen_buffer
+		if @offscreen_buffer
+			update_offscreen_buffer! if update_offscreen_buffer?
+			@offscreen_buffer.with_image {
+				yield
+			}
+		end
+	end
+
+	def update_offscreen_buffer!
+		@offscreen_buffer.using {
+			render!
+		}
 	end
 end
