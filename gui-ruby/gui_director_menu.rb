b6ba03b34bf42f7b8862165890b674aee0c0c5f5
Ian McIntosh <ian@openanswers.org>
2013-06-18 14:22:24 -0700

    - add cancel button to director grid

--- gui-ruby/gui_director_menu.rb
+++ gui-ruby/gui_director_menu.rb
@@ -10,11 +10,16 @@ class GuiDirectorMenu < GuiBox
 
 		self << @grid = GuiGrid.new.set(:scale => 0.95, :spacing_x => 0.1, :spacing_y => 0.1)
 
-		self << @add_button = GuiButton.new.set(:scale => 0.10, :offset_x => 0.45, :offset_y => -0.45, :background_image => $engine.load_image('images/buttons/add.png'))
+		self << @add_button = GuiButton.new.set(:scale => 0.05, :offset_x => -0.475, :offset_y => -0.475, :background_image => $engine.load_image('images/buttons/add.png'))
 		@add_button.on_clicked {
 			director = Director.new
 			@grid << director
 			$gui.build_editor_for(director)
+		}
+
+		self << @cancel_button = GuiButton.new.set(:scale => 0.05, :offset_x => 0.475, :offset_y => 0.475, :background_image => $engine.load_image('images/buttons/close.png'))
+		@cancel_button.on_clicked {
+			$gui.build_editor_for($gui.chosen_director)
 		}
 	end
 end
