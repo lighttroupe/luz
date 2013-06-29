b0edd4f2bc6a6a00cb6e2fc315433f333158a6a3
Ian McIntosh <ian@openanswers.org>
2013-06-28 20:49:30 -0700

    - VariableFlyout buttons, New Event button in center

--- gui-ruby/gui_variables_flyout.rb
+++ gui-ruby/gui_variables_flyout.rb
@@ -9,24 +9,24 @@ class GuiVariablessFlyout < GuiBox
 		self << (@background = GuiObject.new.set(:background_image => $engine.load_image('images/variables-flyout-background.png')))
 
 		# Events list				TODO: don't use $engine here
-		self << @events_list = GuiList.new($engine.project.events).set(:scale_x => 0.91, :scale_y => 0.38, :offset_x => -0.036, :offset_y => 0.22, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
+		self << @events_list = GuiList.new($engine.project.events).set(:scale_x => 0.91, :scale_y => 0.37, :offset_x => -0.036, :offset_y => 0.22, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
 
 		# New Event button
-		self << (@new_event_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.05, :offset_x => 0.0, :offset_y => 0.0, :background_image => $engine.load_image('images/buttons/new-event-button.png')))
+		self << (@new_event_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.045, :offset_x => 0.0, :offset_y => -0.013, :background_image => $engine.load_image('images/buttons/new-event-button.png')))
 		@new_event_button.on_clicked { |pointer|
 			@events_list.add_after_selection(event = Event.new)
 			$gui.build_editor_for(event, :pointer => pointer)
 		}
 
+		# Variables list		TODO: don't use $engine here
+		self << @variables_list = GuiList.new($engine.project.variables).set(:scale_x => 0.91, :scale_y => 0.37, :offset_x => -0.036, :offset_y => -0.23, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
+
 		# New Variable button
-		self << (@new_variable_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.05, :offset_x => 0.0, :offset_y => -0.475, :background_image => $engine.load_image('images/buttons/new-variable-button.png')))
+		self << (@new_variable_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.043, :offset_x => 0.0, :offset_y => -0.476, :background_image => $engine.load_image('images/buttons/new-variable-button.png')))
 		@new_variable_button.on_clicked { |pointer|
 			@variables_list.add_after_selection(variable = Variable.new)
 			$gui.build_editor_for(variable, :pointer => pointer)
 		}
-
-		# Variables list		TODO: don't use $engine here
-		self << @variables_list = GuiList.new($engine.project.variables).set(:scale_x => 0.91, :scale_y => 0.38, :offset_x => -0.036, :offset_y => -0.23, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
 	end
 
 	def variables=(variables)
