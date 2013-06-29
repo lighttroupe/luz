3193b3872fcab6ccfb728f398ac5cd4328f9e87a
Ian McIntosh <ian@openanswers.org>
2013-06-28 23:35:27 -0700

    - engine button/slide aspect ratio

--- gui-ruby/gui_engine_slider.rb
+++ gui-ruby/gui_engine_slider.rb
@@ -7,7 +7,7 @@ class GuiEngineSlider < GuiListSelect
 	def initialize(object, method)		# options is [[:one,'One'],[:two,'Two']]
 		super(object, method)
 		@selected_label = BitmapFont.new.set(:string => get_value, :scale_x => 0.9, :scale_y => 0.65)
-		@item_aspect_ratio = 6.0
+		@item_aspect_ratio = 5.0
 	end
 
 	def list
