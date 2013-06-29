6ae4ed28581fd1b36ab690cf078140291de7557f
Ian McIntosh <ian@openanswers.org>
2013-06-28 20:34:39 -0700

    - VariablesFlyout background and positioning

--- gui-ruby/gui_variables_flyout.rb
+++ gui-ruby/gui_variables_flyout.rb
@@ -8,25 +8,25 @@ class GuiVariablessFlyout < GuiBox
 		# Background
 		self << (@background = GuiObject.new.set(:background_image => $engine.load_image('images/variables-flyout-background.png')))
 
+		# Events list				TODO: don't use $engine here
+		self << @events_list = GuiList.new($engine.project.events).set(:scale_x => 0.91, :scale_y => 0.38, :offset_x => -0.036, :offset_y => 0.22, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
+
 		# New Event button
-		self << (@new_event_button = GuiButton.new.set(:scale_x => 0.45, :scale_y => 0.05, :offset_x => -0.25, :offset_y => -0.45, :background_image => $engine.load_image('images/buttons/new.png')))
+		self << (@new_event_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.05, :offset_x => 0.0, :offset_y => 0.0, :background_image => $engine.load_image('images/buttons/new-event-button.png')))
 		@new_event_button.on_clicked { |pointer|
 			@events_list.add_after_selection(event = Event.new)
 			$gui.build_editor_for(event, :pointer => pointer)
 		}
 
 		# New Variable button
-		self << (@new_variable_button = GuiButton.new.set(:scale_x => 0.45, :scale_y => 0.05, :offset_x => 0.25, :offset_y => -0.45, :background_image => $engine.load_image('images/buttons/new.png')))
+		self << (@new_variable_button = GuiButton.new.set(:scale_x => 1.0, :scale_y => 0.05, :offset_x => 0.0, :offset_y => -0.475, :background_image => $engine.load_image('images/buttons/new-variable-button.png')))
 		@new_variable_button.on_clicked { |pointer|
 			@variables_list.add_after_selection(variable = Variable.new)
 			$gui.build_editor_for(variable, :pointer => pointer)
 		}
 
-		# Events list				TODO: don't use $engine here
-		self << @events_list = GuiList.new($engine.project.events).set(:scale_x => 1.0, :scale_y => 0.45, :offset_y => 0.22, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
-
 		# Variables list		TODO: don't use $engine here
-		self << @variables_list = GuiList.new($engine.project.variables).set(:scale_x => 1.0, :scale_y => 0.45, :offset_y => -0.23, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
+		self << @variables_list = GuiList.new($engine.project.variables).set(:scale_x => 0.91, :scale_y => 0.38, :offset_x => -0.036, :offset_y => -0.23, :item_aspect_ratio => 3.2, :spacing_y => -1.0)
 	end
 
 	def variables=(variables)
