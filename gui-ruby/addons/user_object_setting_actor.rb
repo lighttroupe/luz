f11a589b1e7259c867f7ba323dcf1ed84f9e70e2
Ian McIntosh <ian@openanswers.org>
2013-07-05 17:43:53 -0700

    - UserObjectSettingActor shouldn't stretch to fill x

--- gui-ruby/addons/user_object_setting_actor.rb
+++ gui-ruby/addons/user_object_setting_actor.rb
@@ -2,7 +2,7 @@ class UserObjectSettingActor
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiActor.new(self, :actor)
+		box << GuiActor.new(self, :actor).set(:offset_x => -0.4, :scale_x => 0.2)
 		box
 	end
 end
