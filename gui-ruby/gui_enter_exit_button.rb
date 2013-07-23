82d98a969e9808e67d52ce3cce036e685a6d5123
Ian McIntosh <ian@openanswers.org>
2013-07-22 20:28:50 -0700

    - enable_exit => enable_exit_animation

--- gui-ruby/gui_enter_exit_button.rb
+++ gui-ruby/gui_enter_exit_button.rb
@@ -11,7 +11,7 @@ class GuiEnterExitButton < GuiButton
 		with_gui_object_properties {
 			with_translation(-0.25, 0.0) {
 				with_scale(0.5, 1.0) {
-					with_alpha(@object.enable_enter ? 1.0 : 0.4) {
+					with_alpha(@object.enable_enter_animation ? 1.0 : 0.4) {
 						enter_image.using {
 							unit_square
 						}
@@ -20,7 +20,7 @@ class GuiEnterExitButton < GuiButton
 			}
 			with_translation(0.25, 0.0) {
 				with_scale(0.5, 1.0) {
-					with_alpha(@object.enable_exit ? 1.0 : 0.4) {
+					with_alpha(@object.enable_exit_animation ? 1.0 : 0.4) {
 						exit_image.using {
 							unit_square
 						}
