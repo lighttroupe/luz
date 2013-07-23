a23faa1859d08908939843909c53cfd4e1663226
Ian McIntosh <ian@openanswers.org>
2013-07-22 21:41:00 -0700

    - comments

--- utils/easy_accessor.rb
+++ utils/easy_accessor.rb
@@ -4,11 +4,12 @@ class Module
 		if signals.size == 2 && (options = signals.last).is_a?(Hash)
 			signal = signals.first
 			default = options[:default]
-			puts "def #{signal}() ; @#{signal} || (@#{signal}_default_#{Time.now.to_i} ||= #{default.inspect}) ; end"
+			puts "def #{signal}() ; @#{signal} || (@#{signal}_default_#{Time.now.to_i} ||= #{default.inspect}) ; end"		# using Time breaks cache on code reload
 			module_eval( "def #{signal}() ; @#{signal} || (@#{signal}_default_#{Time.now.to_i} ||= #{default.inspect}) ; end", __FILE__, __LINE__ )
 			module_eval( "def #{signal}=(value) @#{signal} = value ; end", __FILE__, __LINE__ )
 			module_eval( "def set_#{signal}(value) @#{signal} = value ; self ; end", __FILE__, __LINE__ )
 		else
+			# easy_accessor :one, :two, :three
 			signals.each { |signal|
 				module_eval( "def #{signal}() ; @#{signal} ; end", __FILE__, __LINE__ )
 				module_eval( "def #{signal}=(value) @#{signal} = value ; end", __FILE__, __LINE__ )
