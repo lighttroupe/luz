f281ac885cc9fa4c8b736f629bc350dfba134f93
Josh Fitz <joshfitzart@joshfitzmac.(none)>
2013-12-29 19:57:33 -0800

    - quit button image

--- gui-ruby/gui_main_menu.rb
+++ gui-ruby/gui_main_menu.rb
@@ -13,7 +13,7 @@ class GuiMainMenu < GuiBox
 		self << @save_button = GuiButton.new.set(:scale => 0.05, :offset_x => 0.475, :offset_y => 0.475, :background_image => $engine.load_image('images/buttons/save.png'))
 		@save_button.on_clicked { save_notify ; close_notify }
 
-		self << @quit_button = GuiButton.new.set(:scale => 0.05, :offset_x => -0.475, :offset_y => -0.475, :background_image => $engine.load_image('images/buttons/menu.png'))
+		self << @quit_button = GuiButton.new.set(:scale => 0.05, :offset_x => -0.475, :offset_y => -0.475, :background_image => $engine.load_image('images/buttons/quit.png'))
 		@quit_button.on_clicked { $application.finished! }
 	end
 end
