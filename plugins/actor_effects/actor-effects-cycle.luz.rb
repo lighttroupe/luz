94712e9344cbe3a703e62bdb089e638b60045186
Ian McIntosh <ian@openanswers.org>
2013-12-29 19:47:16 -0800

    - and and

--- plugins/actor_effects/actor-effects-cycle.luz.rb
+++ plugins/actor_effects/actor-effects-cycle.luz.rb
@@ -34,18 +34,17 @@ class ActorEffectActorEffectsCycle < ActorEffect
 
 	def render
 		@current_spot = cycle_update(@current_spot, (forwards.count - backwards.count), crossfade_time)
-
 		low_index = @current_spot.floor
 		first, second, progress = actors.one(low_index), actors.one(low_index+1), (@current_spot - low_index)
-		use_first = (first and (progress != 1.0))
-		use_second = (second and (progress != 0.0))
+		use_first = (first && (progress != 1.0))
+		use_second = (second && (progress != 0.0))
 		if use_first
 			with_enter_and_exit(1.0, progress) {
 				first.render_recursive {
 					if use_second
 						with_enter_and_exit(progress, 0.0) {
 							second.render_recursive { yield }
-						} 
+						}
 					else
 						yield
 					end
