64c0c3958478ce1aff730bc83667d515b70fd12e
Ian McIntosh <ian@openanswers.org>
2014-02-28 03:22:12 -0800

    - add each_with_extensions

--- utils/addons_ruby.rb
+++ utils/addons_ruby.rb
@@ -665,6 +665,10 @@ class Dir
 		each { |filename| yield File.join(path, filename) if File.fnmatch(pattern, filename) }
 	end
 
+	def each_with_extensions(extensions)		# accepts array of extensions eg. ['png', 'gif', 'jpg', 'jpeg']
+		each { |filename| yield File.join(path, filename) if extensions.nil? || extensions.include?(File.extname(filename).sub(/^\./,'')) }
+	end
+
 	def each_matching_recursive(pattern)	# accepts patterns like '*.png'
 		each { |filename|
 			unless filename == '..' or filename == '.'
