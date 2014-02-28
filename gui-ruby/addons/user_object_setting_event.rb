939032bdf60dd1b65962043d3be5da0a0418f569
Ian McIntosh <ian@openanswers.org>
2014-02-28 04:00:38 -0800

    - fix aspect ratio of event select list

--- gui-ruby/addons/user_object_setting_event.rb
+++ gui-ruby/addons/user_object_setting_event.rb
@@ -2,7 +2,7 @@ class UserObjectSettingEvent
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiEvent.new(self, :event)
+		box << GuiEvent.new(self, :event).set(:item_aspect_ratio => 5.0)
 		box
 	end
 end
