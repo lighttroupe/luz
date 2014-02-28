202ffd1e14c6e5e4790a57073236800d4e5e3c0d
Ian McIntosh <ian@openanswers.org>
2014-02-28 02:31:49 -0800

    - nil error prevention

--- plugins/variable_inputs/button-toggle.luz.rb
+++ plugins/variable_inputs/button-toggle.luz.rb
@@ -28,10 +28,10 @@ class VariableInputButtonToggle < VariableInput
 
 	def value
 		if $engine.button_press_count(button).is_odd?
-			return 1.0 if on_time.instant?
+			return 1.0 if on_time.instant? or last_value.nil?
 			return (last_value + ($env[:frame_time_delta] / on_time.to_seconds)).clamp(0.0, 1.0)
 		else
-			return 0.0 if off_time.instant?
+			return 0.0 if off_time.instant? or last_value.nil?
 			return (last_value - ($env[:frame_time_delta] / off_time.to_seconds)).clamp(0.0, 1.0)
 		end
 	end
