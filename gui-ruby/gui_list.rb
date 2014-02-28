8136bd5bf1d04f85abc5ee4c04ec5b9d34989d7e
Ian McIntosh <ian@openanswers.org>
2014-02-28 02:58:12 -0800

    - hacky alt-color rows

--- gui-ruby/gui_list.rb
+++ gui-ruby/gui_list.rb
@@ -54,10 +54,16 @@ class GuiList < GuiBox
 	end
 
 	BACKGROUND_COLOR = [0,0,0,0.8]
+	ALT_COLOR = [0.1,0.1,0.12,0.7]
 	def gui_render!
 		return if hidden?
 #		with_positioning { with_color(BACKGROUND_COLOR) { unit_square } }
-		each_with_positioning { |gui_object| gui_object.gui_render! }
+		toggle = false		# TODO: improve
+		each_with_positioning { |gui_object|
+			with_color(ALT_COLOR) { unit_square } if toggle
+			toggle = !toggle
+			gui_object.gui_render!
+		}
 
 		if keyboard_focus?
 			with_positioning {
