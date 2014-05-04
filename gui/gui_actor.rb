6b7acf03a99692f770f02d3644ddfc8984e2ecfb
Ian McIntosh <ian@openanswers.org>
2014-05-04 03:52:52 -0700

    - show project actor list in GuiActor

--- gui/gui_actor.rb
+++ gui/gui_actor.rb
@@ -2,6 +2,11 @@ require 'gui_list_select'
 
 class GuiActor < GuiListSelect
 	def list
-		$engine.project.actors
+		#actors = [] ; ObjectSpace.each_object(Actor) { |a| actors << a }
+		$engine.project.actors.map { |actor| GuiObjectRenderer.new(actor) }
+	end
+
+	def set_value(value)
+		super(value.object)
 	end
 end
