ff871b3073453ab660754166d881348d7b05f765
Ian McIntosh <ian@openanswers.org>
2014-05-12 10:38:02 -0700

    - cleanup constants.rb

--- constants.rb
+++ constants.rb
@@ -1,26 +1,22 @@
-# Time constants
+# Defaults
+MESSAGE_BUS_IP			= "255.255.255.255"		# Broadcast address
+MESSAGE_BUS_PORT		= 10007								# LOOOZ ! :D
+
+SETTINGS_DIRECTORY = '.luz'
+SETTINGS_FILENAME = 'settings.yaml'
+
+# Time
 SECONDS_PER_MINUTE	= 60
 SECONDS_PER_HOUR		= 3600
 
-# Radian constants
+# Radians
 RADIANS_PER_CIRCLE	= Math::PI * 2.0
 RADIANS_UP					= -Math::PI / 2.0
 RADIANS_RIGHT				= 0.0
 RADIANS_DOWN				= Math::PI / 2.0
 RADIANS_LEFT				= Math::PI
 
-# Degree constants
+# Degrees
 DEGREES_PER_CIRCLE	= 360.0
-
 RADIANS_TO_DEGREES	= (360.0 / RADIANS_PER_CIRCLE)
-FUZZY_TO_DEGREES		= -360.0				# multiply to produce 0 = up, 0.25 = right, etc.
-
-#MESSAGE_BUS_IP			= "225.4.5.6"		# Multicast address (http://onestepback.org/index.cgi/Tech/Ruby/MulticastingInRuby.red)
-
-MESSAGE_BUS_IP			= "255.255.255.255"		# Broadcast address
-MESSAGE_BUS_PORT		= 10007					# LOOOZ ! :D
-
-SETTINGS_DIRECTORY = '.luz'
-SETTINGS_FILENAME = 'settings.yaml'
-
-ENABLE_DRAWABLE_LIST_OPTIMIZATIONS = true
+FUZZY_TO_DEGREES		= -360.0							# multiply to produce 0 = up, 0.25 = right, etc.
