58edfeaa5e1e941d489d7b5041d3a1fc22d1913f
Ian McIntosh <ian@openanswers.org>
2014-05-12 18:41:56 -0700

    - move add_message_bus into EngineMessageBus

--- engine/engine.rb
+++ engine/engine.rb
@@ -167,17 +167,6 @@ private
 		@project.events.each { |event| event.do_value }
 	end
 
-	#
-	# Message bus
-	#
-	def add_message_bus(ip, port)
-		message_bus = MessageBus.new.listen(ip, port)
-		message_bus.on_button_down(&method(:on_button_down))
-		message_bus.on_button_up(&method(:on_button_up))
-		message_bus.on_slider_change(&method(:on_slider_change))
-		@message_buses << message_bus
-	end
-
 	if optional_require 'rb-inotify'
 		puts 'Using iNotify for live reloading of changed images'
 
--- engine/engine/engine_message_bus.rb
+++ engine/engine/engine_message_bus.rb
@@ -1,4 +1,12 @@
 module EngineMessageBus
+	def add_message_bus(ip, port)
+		message_bus = MessageBus.new.listen(ip, port)
+		message_bus.on_button_down(&method(:on_button_down))
+		message_bus.on_button_up(&method(:on_button_up))
+		message_bus.on_slider_change(&method(:on_slider_change))
+		@message_buses << message_bus
+	end
+
 	def read_from_message_bus
 		@message_buses.each { |bus| bus.update }
 	end
