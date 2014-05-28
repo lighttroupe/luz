875b173ff180d0d61c7952e77edd177c62fa9ee7
Ian McIntosh <ian@openanswers.org>
2014-05-28 14:47:04 -0700

    - remove outdated hint

--- engine/plugins/project_effects/stop-performer.luz.rb
+++ engine/plugins/project_effects/stop-performer.luz.rb
@@ -2,8 +2,6 @@ class ProjectEffectStopPerformer < ProjectEffect
 	title				"Stop Performer"
 	description "Quits fullscreen performance mode on a chosen event."
 
-	hint "Also capable of quiting the Video Renderer."
-
 	setting 'event', :event, :summary => 'on %'
 
 	def tick
