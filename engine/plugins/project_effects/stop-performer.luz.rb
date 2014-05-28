f592ae830cba3eb2eb6cb55196266cafbf90d1e1
Ian McIntosh <ian@openanswers.org>
2014-05-28 14:43:14 -0700

    - application always responds to finished! now

--- engine/plugins/project_effects/stop-performer.luz.rb
+++ engine/plugins/project_effects/stop-performer.luz.rb
@@ -1,21 +1,3 @@
- ###############################################################################
- #  Copyright 2006 Ian McIntosh <ian@openanswers.org>
- #
- #  This program is free software; you can redistribute it and/or modify
- #  it under the terms of the GNU General Public License as published by
- #  the Free Software Foundation; either version 2 of the License, or
- #  (at your option) any later version.
- #
- #  This program is distributed in the hope that it will be useful,
- #  but WITHOUT ANY WARRANTY; without even the implied warranty of
- #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #  GNU Library General Public License for more details.
- #
- #  You should have received a copy of the GNU General Public License
- #  along with this program; if not, write to the Free Software
- #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- ###############################################################################
-
 class ProjectEffectStopPerformer < ProjectEffect
 	title				"Stop Performer"
 	description "Quits fullscreen performance mode on a chosen event."
@@ -25,6 +7,6 @@ class ProjectEffectStopPerformer < ProjectEffect
 	setting 'event', :event, :summary => 'on %'
 
 	def tick
-		$application.finished! if (event.now? and $application.respond_to? :finished!)
+		$application.finished! if event.now?
 	end
 end
