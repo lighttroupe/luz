8d1562099d40152c9af10f656c2e61b98bb408b7
Ian McIntosh <ian@openanswers.org>
2014-05-28 16:22:43 -0700

    - cleanup, remove copyright notice

--- spectrum-analyzer/spectrum-analyzer.cc
+++ spectrum-analyzer/spectrum-analyzer.cc
@@ -1,22 +1,3 @@
-/*
- *   Copyright 2009 Ian McIntosh <ian@openanswers.org>
- *
- *   This program is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU General Public License
- *   as published by the Free Software Foundation; either version 2
- *   of the License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
 #include "spectrum-analyzer.h"
 #include "utils.h"
 #include "filter.h"
@@ -34,15 +15,12 @@ using namespace std;    // saves us typing std:: before vector
 
 #define APPLICATION_NAME ("Luz Spectrum Analyzer")
 #define APPLICATION_VERSION ("0.31")
-#define APPLICATION_COPYRIGHT ("Copyright 2008 Ian McIntosh")
 
-//
 // liblo OpenSoundControl (http://liblo.sourceforge.net/)
-//
 #include <lo/lo.h>
 
-#define UDP_PORT ("10007") // LOOOZ :D
-#define UDP_ADDRESS NULL //("255.255.255.255")
+#define UDP_PORT ("10007")		// LOOOZ :D
+#define UDP_ADDRESS (NULL)		// ("255.255.255.255")
 
 #define ADDRESS_BUFFER_SIZE 1000
 char address_buffer[ADDRESS_BUFFER_SIZE + 1];
@@ -56,8 +34,6 @@ bool g_time_to_quit = false;
 
 #include "message-bus.h"
 #include "spectrum-analyzer-window.h"
-
-#define APPLICATION_NAME ("Luz Spectrum Analyzer")
 
 #define RC_FILE_PATH ("spectrum-analyzer.rc")
 #define PNG_ICON_FILE_PATH ("spectrum-analyzer-status-icon.png")
