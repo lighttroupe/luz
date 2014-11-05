0fa083d9c3e072b94f199893de80e3c9a1cd416e
Ian McIntosh <ian@openanswers.org>
2014-11-06 01:30:41 +0900

    - remove unused constants

--- engine/user_object_settings/user_object_setting_button.rb
+++ engine/user_object_settings/user_object_setting_button.rb
@@ -1,9 +1,6 @@
 require 'user_object_setting'
 
 class UserObjectSettingButton < UserObjectSetting
-	LABEL_NORMAL = 'Record '
-	LABEL_RECORDING = 'Recording - press desired button'
-
 	def to_yaml_properties
 		super + ['@button']
 	end
