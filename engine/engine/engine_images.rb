408ec06f286ff7db3549902d1de9983d236a4251
Ian McIntosh <ian@openanswers.org>
2014-11-05 10:10:22 +0900

    - code cleanup in EngineImages

--- engine/engine/engine_images.rb
+++ engine/engine/engine_images.rb
@@ -23,16 +23,14 @@ module EngineImages
 		}
 	end
 
-	def load_images(path)
-		path = path.sub(@project.file_path, '') if @project.file_path && path.index(@project.file_path) == 0
+	def load_images(relative_path)
+		@images_cache ||= {}
 
-		# supports absolute paths, or relative paths from one of the registered image directories
-		file_path = ([path] + image_directories.map { |dir| File.join(dir, path) }).find { |p|
-			File.exists?(p)
-		}
+		relative_path = relative_path.without_prefix(@project.file_path)		# path.sub(, '') if @project.file_path && path.index(@project.file_path) == 0
+
+		file_path = find_file_by_relative_path(relative_path)
 
 		# Note: cache using the full path name, so that two projects with similar relative paths won't get confused
-		@images_cache ||= {}
 		return @images_cache[file_path] if @images_cache[file_path]
 
 		unless file_path && File.exists?(file_path)
@@ -40,42 +38,46 @@ module EngineImages
 			return (@images_cache[file_path] ||= [Image.new])
 		end
 
-		ret = nil
+		return_value = nil
 		with_watch(file_path) {
 			@images_cache[file_path] ||= []		# DO NOT replace with [] as that would invalidate external object references
 
-			timer("load #{path}", :if_over => 0.1) {
+			timer("load #{relative_path}", :if_over => 0.1) {
 				case File.extname(file_path).downcase
 				when '.png', '.jpg', '.jpeg', '.bmp'
 					@images_cache[file_path][0] ||= Image.new
 					@images_cache[file_path][0].from_image_file_path(file_path)
-					ret = @images_cache[file_path]
+					return_value = @images_cache[file_path]
 				when '.gif'
 					begin
 						list = Magick::ImageList.new(file_path).coalesce
 						image_list = @images_cache[file_path]
 						list.each_with_index { |rmagick_image, index| image_list[index] ||= Image.new ; image_list[index].load_from_rmagick_image(rmagick_image) }
-						ret = image_list
+						return_value = image_list
 					rescue
-						ret = false
+						return_value = false
 					end
 				else
 					puts "unhandled image file type for #{file_path}"
-					ret = false
+					return_value = false
 				end
 			}
 			true		# tell with_watch that we were successful
 		}
-		return ret
+		return_value
 	end
 
 private
 
+	# given an absolute or relative path, find an existing file in one of the registered image directories
+	def find_file_by_relative_path(relative_path)
+		([relative_path] + image_directories.map { |dir| File.join(dir, relative_path) }).find { |p|
+			File.exists?(p)
+		}
+	end
+
 	def image_directories
-		[
-			@project.file_path,
-			'gui'
-		].compact
+		[@project.file_path, 'gui'].compact
 	end
 
 	def thumbnailer
