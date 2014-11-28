9014fd321a0ab3aa4dc19de1b7b13661f17c552c
Ian McIntosh <ian@openanswers.org>
2014-11-28 04:14:02 -0800

    - String should call on_change callbacks

--- gui/addons/user_object_setting_string.rb
+++ gui/addons/user_object_setting_string.rb
@@ -7,4 +7,8 @@ class UserObjectSettingString
 		box << GuiString.new(self, :string).set(:scale_x => 0.5, :float => :left, :scale_y => 0.5, :offset_y => 0.0)
 		box
 	end
+
+	def string=(s)
+		set(:string, s)			# this includes callbacks
+	end
 end
