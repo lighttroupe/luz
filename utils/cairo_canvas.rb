6b82099c1bd79e6d0489ac0d781c5177d51b7b6d
Ian McIntosh <ian@openanswers.org>
2014-11-28 17:47:21 -0800

    - rename @cr to @context

--- utils/cairo_canvas.rb
+++ utils/cairo_canvas.rb
@@ -1,7 +1,7 @@
 require 'cairo_context'
 
 class CairoCanvas
-	attr_reader :width, :height, :string_data
+	attr_reader :width, :height, :string_data, :context
 
 	def initialize(width, height)
 		@width, @height = width, height
@@ -9,17 +9,17 @@ class CairoCanvas
 		@string_data = ("\0" * (@width * @height * 4))		# TODO: ensure this is right for RUBY1.9, RUBY2.0 etc.
 		@cairo_surface = Cairo::ImageSurface.new(@string_data, Cairo::FORMAT_ARGB32, @width, @height, stride = (@width * 4))
 
-		@cr = Cairo::Context.new(@cairo_surface)
-		@cr.paint
+		@context = Cairo::Context.new(@cairo_surface)
+		@context.paint
 	end
 
-	pipe :track_dirty_rects=, :cr
-	pipe :dirty_rects, :cr
-	pipe :dirty_rects_clear, :cr
-	pipe :entire_canvas_dirty?, :cr
-	pipe :entire_canvas_dirty=, :cr
+	pipe :track_dirty_rects=, :context
+	pipe :dirty_rects, :context
+	pipe :dirty_rects_clear, :context
+	pipe :entire_canvas_dirty?, :context
+	pipe :entire_canvas_dirty=, :context
 
 	def using
-		yield @cr
+		yield @context
 	end
 end
