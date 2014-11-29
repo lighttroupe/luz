0908f63988f06135bfaddd1f49f9d1b439d02bef
Ian McIntosh <ian@openanswers.org>
2014-11-28 17:47:45 -0800

    - working font list, selection

--- gui/addons/user_object_setting_font.rb
+++ gui/addons/user_object_setting_font.rb
@@ -2,15 +2,22 @@ class UserObjectSettingFont
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiSelect.new(self, :selected, options).set(:scale_x => 1.0, :scale_y => 0.5, :offset_y => 0.25)
+		box << GuiSelect.new(self, :font, options).set(:scale_x => 1.0, :scale_y => 0.5, :offset_y => 0.25)
 		box
 	end
 
 private
 
 	def options
-		#@context ||= Pango::Context.new
-		#p @context.families
-		[[:one, 'Font One'], [:one, 'Font Two']]
+		unless @font_options
+			canvas = CairoCanvas.new(0, 0)
+			canvas.using { |context|
+				layout = context.create_pango_layout
+				pango_context = layout.context
+				font_families = pango_context.font_map.families.map(&:name).sort
+				@font_options = font_families.map { |name| [name, name] }
+			}
+		end
+		@font_options
 	end
 end
