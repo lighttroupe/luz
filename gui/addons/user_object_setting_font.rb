20aca653aeddd788e92486d3a338a34c26dde934
Ian McIntosh <ian@openanswers.org>
2014-11-28 19:15:30 -0800

    - add GuiFontSelect and use it in UserObjectSettingFont

--- gui/addons/user_object_setting_font.rb
+++ gui/addons/user_object_setting_font.rb
@@ -2,22 +2,7 @@ class UserObjectSettingFont
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiSelect.new(self, :font, options).set(:scale_x => 1.0, :scale_y => 0.5, :offset_y => 0.25)
+		box << GuiFontSelect.new(self, :font).set(:scale_x => 1.0, :scale_y => 0.5, :offset_y => 0.25, :item_aspect_ratio => 6.0)
 		box
-	end
-
-private
-
-	def options
-		unless @font_options
-			canvas = CairoCanvas.new(0, 0)
-			canvas.using { |context|
-				layout = context.create_pango_layout
-				pango_context = layout.context
-				font_families = pango_context.font_map.families.map(&:name).sort
-				@font_options = font_families.map { |name| [name, name] }
-			}
-		end
-		@font_options
 	end
 end
---
+++ gui/gui_font_select.rb
@@ -0,0 +1,33 @@
+class GuiFontSelect < GuiListSelect
+	def list
+		$gui_font_select_options ||= build_available_font_family_list
+		$gui_font_select_options.map { |name| GuiTextRenderer.new(name) }
+	end
+
+	def build_available_font_family_list
+		canvas = CairoCanvas.new(0, 0)
+		canvas.using { |context|
+			layout = context.create_pango_layout
+			pango_context = layout.context
+			return pango_context.font_map.families.map(&:name).sort
+		}
+		[]
+	end
+
+	def set_value(value)
+		super(value.text)
+	end
+
+	#
+	# Render
+	#
+	def gui_render!
+		font_name = get_value
+		return super unless font_name		# "none" text
+		with_gui_object_properties {
+			@value_label ||= BitmapFont.new.set(:scale => 0.75)
+			@value_label.set_string(font_name)
+			@value_label.gui_render!
+		}
+	end
+end
