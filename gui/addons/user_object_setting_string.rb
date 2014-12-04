3c968b2286106eb5b9d7db39fd8b5653df24739f
Ian McIntosh <ian@openanswers.org>
2014-12-04 05:53:01 -0800

    - set width of UserObjectSettingString

--- gui/addons/user_object_setting_string.rb
+++ gui/addons/user_object_setting_string.rb
@@ -4,7 +4,7 @@ class UserObjectSettingString
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiString.new(self, :string).set(:scale_x => 0.5, :float => :left, :scale_y => 0.5, :offset_y => 0.0)
+		box << GuiString.new(self, :string).set(:width => 20, :scale_x => 1.0, :float => :left, :scale_y => 0.5, :offset_y => 0.0)
 		box
 	end
 
