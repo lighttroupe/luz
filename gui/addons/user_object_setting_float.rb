3a44141b5c6cb3b3e4cb361c4d12ee7cbace4d31
Ian McIntosh <ian@openanswers.org>
2014-12-08 03:04:12 -0800

    - trying UOSFloat controls hidden unless enabled

--- gui/addons/user_object_setting_float.rb
+++ gui/addons/user_object_setting_float.rb
@@ -28,19 +28,19 @@ private
 
 			unless @setting.options[:simple]
 				row << (@enable_animation_toggle=GuiToggle.new(@setting, :enable_animation).set(:scale_x => 0.07, :float => :left, :color => [1,0,0,1], :image => $engine.load_image('images/buttons/play.png')))
-				row << (@animation_curve_widget=GuiCurve.new(@setting, :animation_curve).set(:scale_x => 0.13, :scale_y => 0.8, :float => :left, :opacity => 0.4))
-				row << (@animation_max_widget=GuiFloat.new(@setting, :animation_max, @min, @max).set(:scale_x => 0.15, :float => :left, :opacity => 0.4))
-				row << (@animation_every_text=GuiLabel.new.set(:width => 4, :text_align => :center, :string => 'every', :offset_x => 0.025, :scale_x => 0.1, :float => :left, :opacity => 0.4))
-				row << (@animation_repeat_number_widget=GuiFloat.new(@setting, :animation_repeat_number, 0.25, 128.0).set(:step_amount => 0.25, :scale_x => 0.2, :float => :left, :opacity => 0.4))
-				row << (@animation_repeat_unit_widget=GuiSelect.new(@setting, :animation_repeat_unit, UserObjectSettingFloat::TIME_UNIT_OPTIONS).set(:width => 5, :text_align => :left, :scale_x => 0.15, :float => :left, :opacity => 0.4))
+				row << (@animation_curve_widget=GuiCurve.new(@setting, :animation_curve).set(:scale_x => 0.13, :scale_y => 0.8, :float => :left, :opacity => 0.0, :hidden => true))
+				row << (@animation_max_widget=GuiFloat.new(@setting, :animation_max, @min, @max).set(:scale_x => 0.15, :float => :left, :opacity => 0.0, :hidden => true))
+				row << (@animation_every_text=GuiLabel.new.set(:width => 4, :text_align => :center, :string => 'every', :offset_x => 0.025, :scale_x => 0.1, :float => :left, :opacity => 0.0, :hidden => true))
+				row << (@animation_repeat_number_widget=GuiFloat.new(@setting, :animation_repeat_number, 0.25, 128.0).set(:step_amount => 0.25, :scale_x => 0.2, :float => :left, :opacity => 0.0, :hidden => true))
+				row << (@animation_repeat_unit_widget=GuiSelect.new(@setting, :animation_repeat_unit, UserObjectSettingFloat::TIME_UNIT_OPTIONS).set(:width => 5, :text_align => :left, :scale_x => 0.15, :float => :left, :opacity => 0.0, :hidden => true))
 
 				@animation_widgets = [@animation_curve_widget, @animation_max_widget, @animation_every_text, @animation_repeat_number_widget, @animation_repeat_unit_widget]
 
 				@enable_animation_toggle.on_clicked_with_init {
 					if @enable_animation_toggle.on?
-						@animation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 1.0}, duration = (0.05 + (index * 0.2))) }
+						@animation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 1.0, :hidden => false}, duration = (0.05 + (index * 0.3))) }
 					else
-						@animation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 0.2}, duration = (0.05 + (index * 0.1))) }
+						@animation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 0.05, :hidden => true}, duration = (0.2)) }
 					end
 				}
 			end
@@ -77,20 +77,20 @@ private
 			}
 
 			row << (@enable_activation_toggle=GuiToggle.new(@setting, :enable_activation).set(:scale_x => 0.07, :float => :left, :offset_x => 0.15, :color => [1,0,0,1], :image => $engine.load_image('images/buttons/play.png')))
-			row << (@activation_curve_widget=GuiCurveIncreasing.new(@setting, :activation_curve).set(:scale_x => 0.13, :scale_y => 0.8, :float => :left, :opacity => 0.4))
-			row << (@activation_direction_widget=GuiSelect.new(@setting, :activation_direction, UserObjectSettingFloat::ACTIVATION_DIRECTION_OPTIONS).set(:width => 4, :text_align => :center, :scale_x => 0.1, :float => :left, :opacity => 0.4))
-			row << (@activation_value_widget=GuiFloat.new(@setting, :activation_value, @min, @max).set(:scale_x => 0.15, :float => :left, :opacity => 0.4))
+			row << (@activation_curve_widget=GuiCurveIncreasing.new(@setting, :activation_curve).set(:scale_x => 0.13, :scale_y => 0.8, :float => :left, :opacity => 0.0, :hidden => true))
+			row << (@activation_direction_widget=GuiSelect.new(@setting, :activation_direction, UserObjectSettingFloat::ACTIVATION_DIRECTION_OPTIONS).set(:width => 4, :text_align => :center, :scale_x => 0.1, :float => :left, :opacity => 0.0, :hidden => true))
+			row << (@activation_value_widget=GuiFloat.new(@setting, :activation_value, @min, @max).set(:scale_x => 0.15, :float => :left, :opacity => 0.0, :hidden => true))
 
-			row << (@activation_when_text=GuiLabel.new.set(:width => 4, :text_align => :center, :string => 'when', :scale_x => 0.1, :float => :left, :opacity => 0.4))
-			row << (@activation_variable_widget=GuiVariable.new(@setting, :activation_variable).set(:scale_x => 0.26, :float => :left, :opacity => 0.4, :no_value_text => 'variable'))
+			row << (@activation_when_text=GuiLabel.new.set(:string => 'when', :width => 4, :text_align => :center, :scale_x => 0.1, :float => :left, :opacity => 0.0, :hidden => true))
+			row << (@activation_variable_widget=GuiVariable.new(@setting, :activation_variable).set(:no_value_text => 'variable', :scale_x => 0.26, :float => :left, :opacity => 0.0, :hidden => true))
 
 			@activation_widgets = [@activation_curve_widget, @activation_value_widget, @activation_direction_widget, @activation_when_text, @activation_variable_widget]
 
 			@enable_activation_toggle.on_clicked_with_init {
 				if @enable_activation_toggle.on?
-					@activation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 1.0}, duration = (0.05 + (index * 0.2))) }
+					@activation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 1.0, :hidden => false}, duration = (0.05 + (index * 0.3))) }
 				else
-					@activation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 0.2}, duration = (0.05 + (index * 0.1))) }
+					@activation_widgets.each_with_index { |widget, index| widget.animate({:opacity => 0.05, :hidden => true}, duration = (0.2)) }
 				end
 			}
 		end
