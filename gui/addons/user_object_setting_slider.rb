c4e9ffa9401992b62b8a520dd9b73204cc851a7f
Ian McIntosh <ian@openanswers.org>
2014-12-09 01:46:28 -0800

    - UserObjectSettingSlider min/max options

--- gui/addons/user_object_setting_slider.rb
+++ gui/addons/user_object_setting_slider.rb
@@ -1,4 +1,6 @@
 class UserObjectSettingSlider
+	attr_accessor :input_min, :input_max, :output_min, :output_max
+
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
@@ -9,7 +11,17 @@ class UserObjectSettingSlider
 				slider_widget.set_value(slider) unless slider.include?('Mouse')			# HACK: rather impossible to capture anything when spamming inputs like mice		TODO: tablets, wiimotes...
 			}
 		}
-		box << GuiCurveIncreasing.new(self, :output_curve).set(:float => :left, :scale_x => 0.12, :scale_y => 0.7)
+
+		box << (input_min=GuiFloat.new(self, :input_min, 0.0, 1.0).set(:scale_x => 0.07, :scale_y => 0.6, :offset_y => 0.0, :float => :left))
+		box << (input_max=GuiFloat.new(self, :input_max, 0.0, 1.0).set(:scale_x => 0.07, :scale_y => 0.6, :offset_y => 0.0, :float => :left))
+		box << GuiLabel.new.set(:string => 'input', :width => :fill, :color => [0.8,0.8,1.0], :offset_x => 0.13, :scale_x => 0.07, :offset_y => -0.35, :scale_y => 0.3)
+
+		box << GuiCurveIncreasing.new(self, :output_curve).set(:float => :left, :scale_x => 0.12, :scale_y => 0.5)
+
+		box << (output_min=GuiFloat.new(self, :output_min, 0.0, 1.0).set(:scale_x => 0.07, :scale_y => 0.6, :offset_y => 0.0, :float => :left))
+		box << (output_max=GuiFloat.new(self, :output_max, 0.0, 1.0).set(:scale_x => 0.07, :scale_y => 0.6, :offset_y => 0.0, :float => :left))
+		box << GuiLabel.new.set(:string => 'output', :width => :fill, :color => [0.8,0.8,1.0], :offset_x => 0.39, :scale_x => 0.07, :offset_y => -0.35, :scale_y => 0.3)
+
 		box
 	end
 end
