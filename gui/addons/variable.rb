b9eab88079cfe189503cf9b532b69765aa3cc5b7
Ian McIntosh <ian@openanswers.org>
2014-12-12 22:01:08 -0800

    - cleanup / consistency of Variable and VariableInput gui rendering

--- gui/addons/variable.rb
+++ gui/addons/variable.rb
@@ -1,15 +1,15 @@
 class Variable
-	GUI_COLOR = [0.0,1.0,0.5,0.7]
+	PROGRESS_BAR_COLOR = [0.0,1.0,0.5,0.7]
 
 	def gui_render!
 		gui_render_background
-		gui_render_bar(do_value) if enabled?
+		gui_render_progress_bar if enabled?
 		gui_render_label
 	end
 
-	def gui_render_bar(value)
-		with_color(GUI_COLOR) {
-			render_progress_bar_with_cache(value)
+	def gui_render_progress_bar
+		with_color(PROGRESS_BAR_COLOR) {
+			render_progress_bar_with_cache(do_value)
 		}
 	end
 end
--- gui/addons/variable_input.rb
+++ gui/addons/variable_input.rb
@@ -1,17 +1,17 @@
 class VariableInput
-	GUI_COLOR = [0.0,1.0,0.5,0.7]
+	PROGRESS_BAR_COLOR = [0.0,1.0,0.5,0.7]
 
 	def gui_render!
 		gui_render_background
-		gui_render_progress if enabled?
+		gui_render_progress_bar if enabled?
 		gui_render_label
 		gui_render_enable_checkbox
 	end
 
-	def gui_render_progress
+	def gui_render_progress_bar
 		with_translation(enable_checkbox.scale_x / 2.0, 0.0) {
 			with_scale_unsafe(1.0 - enable_checkbox.scale_x, 1.0) {
-				with_color(GUI_COLOR) {
+				with_color(PROGRESS_BAR_COLOR) {
 					render_progress_bar_with_cache(do_value)
 				}
 			}
