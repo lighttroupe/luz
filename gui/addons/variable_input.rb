5681ede033500add3d753f985b7ff5181ea8e6d3
Ian McIntosh <ian@openanswers.org>
2014-12-12 21:58:47 -0800

    - use shared cached progress bar renderer in VariableInput gui addons

--- gui/addons/variable_input.rb
+++ gui/addons/variable_input.rb
@@ -3,25 +3,18 @@ class VariableInput
 
 	def gui_render!
 		gui_render_background
-		with_translation(enable_checkbox.scale_x / 2.0, 0.0) {
-			with_scale_unsafe(1.0 - enable_checkbox.scale_x, 1.0) {
-				gui_render_activation
-			}
-		}
+		gui_render_progress if enabled?
 		gui_render_label
 		gui_render_enable_checkbox
 	end
 
-	def gui_render_activation		# fills 1x1
-		# Status Indicator
-		if enabled? && (v=do_value) > 0.0
-			with_translation(-0.5 + v/2.0, 0.0) {
-				with_scale_unsafe(v, 1.0) {
-					with_color(GUI_COLOR) {
-						unit_square
-					}
+	def gui_render_progress
+		with_translation(enable_checkbox.scale_x / 2.0, 0.0) {
+			with_scale_unsafe(1.0 - enable_checkbox.scale_x, 1.0) {
+				with_color(GUI_COLOR) {
+					render_progress_bar_with_cache(do_value)
 				}
 			}
-		end
+		}
 	end
 end
