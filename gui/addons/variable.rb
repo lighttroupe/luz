5a9d3eb1f94c5afc7263316cac4db2899353d16e
Ian McIntosh <ian@openanswers.org>
2014-12-12 21:34:07 -0800

    - variable gui cleanup

--- gui/addons/variable.rb
+++ gui/addons/variable.rb
@@ -1,54 +1,17 @@
 class Variable
 	GUI_COLOR = [0.0,1.0,0.5,0.7]
-	MARKER_COLOR = [0.8,0.0,0.0,0.15]
 
 	def gui_render!
 		gui_render_background
-
-		value = do_value
-
-		if enabled?
-			render_bar_value_with_cache(value)
-			#render_text_value_with_cache(value)
-			#render_text((100 * value).to_i)
-		end
-
-		# Label
+		render_bar_value_with_cache(do_value) if enabled?
 		gui_render_label
 	end
 
 	def render_bar_value_with_cache(value)
-		cache_key = (1000 * value).to_i
+		cache_key = (1000 * value).to_i		# TODO: 1000 display lists to draw bars might be overkill
 		@@value_list_cache ||= Hash.new
 		@@value_list_cache[cache_key] ||= GL.RenderToList { render_bar(value) }
 		GL.CallList(@@value_list_cache[cache_key])
-	end
-
-	def gui_render_label
-		#with_translation(-0.05, 0.0) {
-		with_translation(0.0, 0.0) {
-			with_scale(1.0, 1.0) {
-				super
-			}
-		}
-	end
-
-	def render_text_value_with_cache(value)
-		@@text_list_cache ||= Hash.new
-
-		cache_key = (100 * value).to_i
-		@@text_list_cache[cache_key] ||= GL.RenderToList { render_text(cache_key) }
-		GL.CallList(@@text_list_cache[cache_key])
-	end
-
-	def render_text(value)
-		@value_label4 ||= GuiLabel.new.set(:width => 3)
-		with_translation(0.44, 0.0) {
-			with_scale(0.16, 0.5) {
-				@value_label4.set_string("#{value}%")
-				@value_label4.gui_render!
-			}
-		}
 	end
 
 	def render_bar(value)
