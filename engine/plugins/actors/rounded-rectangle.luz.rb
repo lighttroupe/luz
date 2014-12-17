27d27d0be67583696343c5982274ff0e36358efb
Ian McIntosh <ian@openanswers.org>
2014-12-17 05:33:26 -0800

    - Rounded Rectangle actor gets inner hole feature

--- engine/plugins/actors/rounded-rectangle.luz.rb
+++ engine/plugins/actors/rounded-rectangle.luz.rb
@@ -5,10 +5,13 @@ class ActorRoundedRectangle < ActorShape
 	setting 'knob_x', :float, :range => 0.0..1.00, :default => 0.25..1.00, :breaks_cache => true
 	setting 'knob_y', :float, :range => 0.0..1.00, :default => 0.25..1.00, :breaks_cache => true
 	setting 'detail', :integer, :range => 1..50, :default => 16..50, :breaks_cache => true		# Points on curve
+	setting 'cutout_size', :float, :range => 0.0..1.00, :default => 0.00..1.0, :breaks_cache => true
 
 	cache_rendering
 
 	def shape
-		yield :shape => Shapes.RoundedRectangle(RADIUS, RADIUS, knob_x, knob_y, detail)
+		s = Shapes.RoundedRectangle(RADIUS, RADIUS, knob_x, knob_y, detail)
+		s = [s, s.dup.multiply_each(cutout_size)] unless cutout_size == 0.0
+		yield :shape => s
 	end
 end
