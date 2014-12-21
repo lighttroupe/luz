dcddbe139d9c6292ed459691044e420525658587
Ian McIntosh <ian@openanswers.org>
2014-12-21 07:19:09 -0800

    - make Hue loop forever

--- engine/plugins/actor_effects/color-hsv.luz.rb
+++ engine/plugins/actor_effects/color-hsv.luz.rb
@@ -4,13 +4,13 @@ class ActorEffectColorHSV < ActorEffect
 
 	categories :color
 
-	setting 'hue', :float, :range => 0.0..1.0, :default => 1.0..1.0
+	setting 'hue', :float, :default => 1.0..1.0
 	setting 'saturation', :float, :range => 0.0..1.0, :default => 1.0..1.0
 	setting 'value', :float, :range => 0.0..1.0, :default => 1.0..1.0
 
 	def render
 		@color ||= Color.new
-		@color.from_hsl(hue, saturation, value)
+		@color.from_hsl(hue % 1.0, saturation, value)
 		with_color(@color) {
 			yield
 		}
