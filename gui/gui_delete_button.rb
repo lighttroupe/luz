78aa0eeb96522492802e11095144496174464651
Ian McIntosh <ian@openanswers.org>
2014-12-31 08:04:08 -0800

    - auto-close delete button cover when pointer leaves

--- gui/gui_delete_button.rb
+++ gui/gui_delete_button.rb
@@ -1,23 +1,39 @@
 class GuiDeleteButton < GuiBox
 	callback :clicked
 
+	class DeleteButton < GuiButton
+		def on_last_pointer_exit
+			parent.close_cover		# auto-close
+		end
+
+		#def gui_render
+			#super
+			#if pointer_hovering?
+				#unit_square
+			#end
+		#end
+	end
+
 	def initialize(*args)
 		super
 		create!
 	end
 
 	def create!
-		self << @button = GuiButton.new.set(:offset_x => 0.25, :scale_x => 0.5, :opacity => 0.5, :background_image => $engine.load_image('images/buttons/delete.png'))
+		self << @button = DeleteButton.new.set(:offset_x => 0.25, :scale_x => 0.5, :opacity => 0.5, :background_image => $engine.load_image('images/buttons/delete.png'), :background_image_hover => $engine.load_image('images/buttons/delete-hover.png'))
 		self << @cover = GuiButton.new.set(:scale_x => 0.5, :opacity => 0.5, :background_image => $engine.load_image('images/buttons/delete-cover.png')).
-						add_state(:open, :offset_x => -0.25).
-						set_state(:closed, :offset_x => 0.25)
+						add_state(:open, :offset_x => -0.25, :skip_hit_test => true).
+						set_state(:closed, :offset_x => 0.25, :skip_hit_test => false)
 
 		@button.on_clicked {
 			clicked_notify
 		}
 		@cover.on_clicked {
-			@cover.switch_state({:closed => :open, :open => :closed}, duration=0.2)
+			@cover.switch_state({:closed => :open}, duration=0.3)
 		}
 	end
-end
 
+	def close_cover
+		@cover.switch_state({:open => :closed}, duration=0.2)
+	end
+end
