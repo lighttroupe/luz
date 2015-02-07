f096ebb24771430debdb5b5ce5ee5248dadb6930
Ian McIntosh <ian@openanswers.org>
2015-02-07 00:28:59 -0800

    - file dialog design

--- gui/gui_file_dialog.rb
+++ gui/gui_file_dialog.rb
@@ -1,12 +1,15 @@
 require 'pathname'
 
+#
+# GuiFileDialog is a file browser/chooser
+#
 class GuiFileDialog < GuiWindow
 	attr_accessor :path
 
 	callback :selected, :closed
 
 	DIRECTORY_COLOR = [0.5,0.5,1.0,1.0]
-	FILE_COLOR = [1,1,1,1]
+	FILE_COLOR = [0.7, 0.96, 0.7]
 
 	def initialize(title, extensions=nil)
 		super()
@@ -37,14 +40,14 @@ class GuiFileDialog < GuiWindow
 
 	def add_directories
 		@directories.each { |filename|
-			@directory_list << (renderer = GuiLabel.new.set(:string => filename, :width => 40, :color => DIRECTORY_COLOR))
+			@directory_list << (renderer = GuiLabel.new.set(:string => filename, :width => 25, :color => DIRECTORY_COLOR))
 			renderer.on_clicked { show_for_path(File.join(@path, filename)) }
 		}
 	end
 
 	def add_files
 		@files.each { |filename|
-			@directory_list << (renderer = GuiLabel.new.set(:string => filename, :width => 40, :color => FILE_COLOR))
+			@directory_list << (renderer = GuiLabel.new.set(:string => filename, :width => 25, :color => FILE_COLOR))
 			renderer.on_clicked { notify_for_filename(filename) }
 		}
 	end
@@ -56,18 +59,21 @@ class GuiFileDialog < GuiWindow
 private
 
 	def create!
-		self << (@background = GuiObject.new.set(:background_image => $engine.load_image('images/file-dialog-background.png')))
+		self << (@overlay = GuiObject.new.set(:background_image => $engine.load_image('images/file-dialog-background.png')))
 
-		self << (@title_label = GuiLabel.new.set({:width => 20, :color => [0.6,0.6,1.0], :string => @title, :offset_x => 0.0, :offset_y => 0.47, :scale_x => 0.3, :scale_y => 0.05}))
+		self << (@background = GuiObject.new.set(:scale_x => 0.3, :color => [0,0,0]))
+
+		self << (@title_label = GuiLabel.new.set({:width => 20, :text_align => :center, :color => [0.6,0.6,1.0], :string => @title, :offset_x => 0.0, :offset_y => 0.47, :scale_x => 0.3, :scale_y => 0.05}))
 
 		#self << (@title_label = GuiLabel.new.set({:color => [0.6,0.6,1.0], :string => @title, :offset_x => -0.03, :offset_y => 0.47, :scale_x => 0.1, :scale_y => 0.05}))
 
-		self << (@up_button=GuiButton.new.set(:scale_x => 0.05, :scale_y => 0.07, :offset_x => -0.452, :offset_y => 0.5 - 0.09, :background_image => $engine.load_image('images/buttons/directory-up.png'), :background_image_hover => $engine.load_image('images/buttons/directory-up-hover.png')))
+		self << (@up_button=GuiButton.new.set(:scale_x => 0.05, :scale_y => 0.07, :offset_x => -0.18, :offset_y => 0.5 - 0.16, :background_image => $engine.load_image('images/buttons/directory-up.png'), :background_image_hover => $engine.load_image('images/buttons/directory-up-hover.png')))
 		@up_button.on_clicked { show_for_path(File.join(@path, '..')) }
-		self << (@path_string = GuiString.new(self, :path).set(:width => 50, :color => [0.7,0.7,0.7], :offset_x => 0.04, :scale_x => 0.9, :scale_y => 0.04, :offset_y => 0.5 - 0.08))
-		self << (@directory_list = GuiList.new.set(:scale_x => 0.95, :scale_y => 0.825, :offset_x => 0.0, :offset_y => -0.035, :spacing_y => -1.0, :item_aspect_ratio => 15.5))
 
-		self << (@close_button=GuiButton.new.set(:scale_x => 0.3, :scale_y => 0.05, :offset_x => 0.0, :offset_y => -0.5 + 0.025, :background_image => $engine.load_image('images/buttons/close-file-view.png'), :background_image_hover => $engine.load_image('images/buttons/close-file-view-hover.png')))
+		self << (@path_string = GuiString.new(self, :path).set(:width => 40, :text_align => :center, :color => [1.0,1.0,1.0], :scale_x => 0.7, :scale_y => 0.04, :offset_y => 0.5 - 0.08))
+		self << (@directory_list = GuiList.new.set(:scale_x => 0.30, :scale_y => 0.825, :offset_x => 0.0, :offset_y => -0.035, :spacing_y => -1.0, :item_aspect_ratio => 8.0))
+
+		self << (@close_button=GuiButton.new.set(:scale_x => 0.06, :scale_y => 0.06, :offset_x => 0.5 - 0.03, :offset_y => 0.5 - 0.03, :background_image => $engine.load_image('images/buttons/close-file-view.png'), :background_image_hover => $engine.load_image('images/buttons/close-file-view-hover.png')))
 		@close_button.on_clicked { closed_notify }
 
 		# focus
