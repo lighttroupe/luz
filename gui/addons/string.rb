d54b59a436c19c71af073f99f1bf3067ea0f8b4d
Ian McIntosh <ian@openanswers.org>
2015-02-07 00:10:55 -0800

    - add String#any_modifiers?

--- gui/addons/string.rb
+++ gui/addons/string.rb
@@ -1,6 +1,9 @@
 class String
 	boolean_accessor :shift, :control, :alt		# modifier keys for user input
 
+	def any_modifiers?
+		shift || control || alt
+	end
 	def no_modifiers?
 		!shift && !control && !alt
 	end
