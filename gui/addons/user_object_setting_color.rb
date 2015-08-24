067c63445dcd26478d6dc41ac2eab5420465ee6e
Ian McIntosh <ian@openanswers.org>
2015-08-23 17:35:53 -0700

    - UserObjectSettingColor ui tweak

--- gui/addons/user_object_setting_color.rb
+++ gui/addons/user_object_setting_color.rb
@@ -3,10 +3,11 @@ class UserObjectSettingColor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
 
-		row = GuiHBox.new		#.set(:scale_y => 0.5, :offset_y => 0.23)
+		row = GuiHBox.new.set(:scale_y => 0.5, :offset_y => 0.15)
 			row << GuiFloat.new(self, :red, 0.0, 1.0, 2).set(:text_align => :center, :scale_x => 0.15, :float => :left)
 			row << GuiFloat.new(self, :green, 0.0, 1.0, 2).set(:text_align => :center, :scale_x => 0.15, :float => :left)
 			row << GuiFloat.new(self, :blue, 0.0, 1.0, 2).set(:text_align => :center, :scale_x => 0.15, :float => :left)
+
 			#row << (@animation_max_widget=GuiFloat.new(self, :animation_max, @min, @max, digits).set(:text_align => :center, :scale_x => 0.15, :float => :left, :opacity => 0.0, :hidden => true))
 			#row << (@animation_every_text=GuiLabel.new.set(:width => 4, :text_align => :fill, :string => 'every', :offset_x => 0.014, :scale_x => 0.08, :float => :left, :opacity => 0.0, :hidden => true))
 
@@ -35,6 +36,6 @@ class UserObjectSettingColor
 	end
 
 	def clear_cache!
-		$gui.user_object.clear_render_styles_cache!
+		$gui.user_object.clear_render_styles_cache! if $gui.user_object && $gui.user_object.respond_to?(:clear_render_styles_cache!)		# hack to update theme icon
 	end
 end
