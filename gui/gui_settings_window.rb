4eb99d68fc41f9c024edf975e086f05082d6b487
Ian McIntosh <ian@openanswers.org>
2016-04-15 02:56:32 -0700

    - proper starting value for frame per second setting

--- gui/gui_settings_window.rb
+++ gui/gui_settings_window.rb
@@ -2,7 +2,10 @@ class GuiSettingsWindow < GuiWindow
 	FRAMES_PER_SECONDS_MIN = 12
 	FRAMES_PER_SECONDS_MAX = 70
 
-	MAX_VALUE_ANIMATION_TIME = 300.0
+	VALUE_ANIMATION_TIME_MIN = 0.1
+	VALUE_ANIMATION_TIME_MAX = 999.9
+
+	DEFAULT_VALUE_ANIMATION_TIME = 10.0
 
 	def initialize
 		super
@@ -18,7 +21,7 @@ class GuiSettingsWindow < GuiWindow
 
 		# value animation time
 		self << GuiLabel.new.set_string("Value Animation Time").set(:width => 13, :text_align => :left, :scale_x => 0.5, :scale_y => 0.06, :offset_x => -0.13, :offset_y => 0.35)
-		self << GuiFloat.new(self, :value_animation_time, 0.1, MAX_VALUE_ANIMATION_TIME, digits=1).set(:width => 3, :offset_x => -0.45, :scale_x => 0.1, :offset_y => 0.345, :scale_y => 0.05)
+		self << GuiFloat.new(self, :value_animation_time, VALUE_ANIMATION_TIME_MIN, VALUE_ANIMATION_TIME_MAX, digits=1).set(:width => 3, :offset_x => -0.45, :scale_x => 0.1, :offset_y => 0.345, :scale_y => 0.05)
 		self << GuiLabel.new.set_string("seconds").set(:color => [0.35,0.35,0.35], :width => 5, :text_align => :right, :scale_x => 0.15, :scale_y => 0.03, :offset_x => -0.39, :offset_y => 0.31)
 
 		# default to value animation
@@ -27,7 +30,7 @@ class GuiSettingsWindow < GuiWindow
 	end
 
 	def frames_per_second
-		$settings['frames-per-second']
+		$settings['frames-per-second'] || $application.frames_per_second
 	end
 	def frames_per_second=(fps)
 		$application.frames_per_second = fps
