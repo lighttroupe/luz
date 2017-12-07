5fd6b3414fd3557e64bc94f4f4a0f49664e89c48
Ian McIntosh <ian@openanswers.org>
2017-12-07 00:32:27 -0800

    - raise stipple segments limit (important when zooming in)

--- engine/plugins/actor_effects/stipple.luz.rb
+++ engine/plugins/actor_effects/stipple.luz.rb
@@ -4,7 +4,7 @@ class ActorEffectStipple < ActorEffect
 
 	categories :color
 
-	setting 'segments', :integer, :range => 1..1000, :default => 100..10000, :shader => true
+	setting 'segments', :integer, :range => 1..5000, :default => 100..10000, :shader => true
 	setting 'size', :float, :range => 0.0..1.0, :default => 1.0..1.0, :shader => true
 
 	CODE = "
