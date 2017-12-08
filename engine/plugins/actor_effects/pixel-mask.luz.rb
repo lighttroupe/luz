960d95b51b8e28dc42326686e5eeec12be4b217f
Ian McIntosh <ian@openanswers.org>
2017-12-08 04:46:58 -0800

    - pixel mask gets inverse mask rule

--- engine/plugins/actor_effects/pixel-mask.luz.rb
+++ engine/plugins/actor_effects/pixel-mask.luz.rb
@@ -4,23 +4,40 @@ class ActorEffectPixelMask < ActorEffect
 
 	categories :color
 
+	setting 'method', :select, :default => :below, :options => [[:above, 'Above'], [:below, 'Below']], :summary => true
+
 	setting 'red_cutoff', :float, :range => 0.0..1.0, :default => 0.0..1.0, :shader => true
 	setting 'green_cutoff', :float, :range => 0.0..1.0, :default => 0.0..1.0, :shader => true
 	setting 'blue_cutoff', :float, :range => 0.0..1.0, :default => 0.0..1.0, :shader => true
 	setting 'alpha_cutoff', :float, :range => 0.0..1.0, :default => 0.0..1.0, :shader => true
 
-	CODE = "
+	BELOW_CODE = "
 		output_rgba *= texture2D(texture0, texture_st);
 		if(output_rgba.a < alpha_cutoff || output_rgba.r < red_cutoff || output_rgba.g < green_cutoff || output_rgba.b < blue_cutoff) {
 			output_rgba = vec4(0,0,0,0);
 		}
 	"
+	ABOVE_CODE = "
+		output_rgba *= texture2D(texture0, texture_st);
+		if(output_rgba.a >= alpha_cutoff && output_rgba.r >= red_cutoff && output_rgba.g >= green_cutoff && output_rgba.b >= blue_cutoff) {
+			output_rgba = vec4(0,0,0,0);
+		}
+	"
+	# NOTE: >= overwise
 
 	def render
-		return yield if (red_cutoff == 0.0 and green_cutoff == 0.0 and blue_cutoff == 0.0 and alpha_cutoff == 0.0)
+		if method == :below
+			return yield if (red_cutoff == 0.0 && green_cutoff == 0.0 && blue_cutoff == 0.0 && alpha_cutoff == 0.0)
 
-		with_fragment_shader_snippet(CODE, self) {
-			yield
-		}
+			with_fragment_shader_snippet(BELOW_CODE, self) {
+				yield
+			}
+		else
+			return yield if (red_cutoff == 1.0 && green_cutoff == 1.0 && blue_cutoff == 1.0 && alpha_cutoff == 1.0)
+
+			with_fragment_shader_snippet(ABOVE_CODE, self) {
+				yield
+			}
+		end
 	end
 end
