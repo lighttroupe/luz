cbd89830ebb3320326f7d6a393681164cd3cd68b
Ian McIntosh <ian@openanswers.org>
2017-12-18 17:32:09 -0800

    - support multiple paths for thumbnails

--- utils/image_thumbnailer.rb
+++ utils/image_thumbnailer.rb
@@ -5,15 +5,25 @@ require 'digest'
 
 class ImageThumbnailer
 	def add(path, &proc)
-		full_path = File.join(thumbnail_directory_path, thumbnail_file_name_for_path(path))
-		proc.call(full_path) if File.exists?(full_path)
+		file_name = thumbnail_file_name_for_path(path)
+		thumbnail_directory_paths.each do |thumbnail_directory_path|
+			full_path = File.join(thumbnail_directory_path, file_name)
+			if File.exists?(full_path)
+				proc.call(full_path)
+				return
+			end
+		end
+
+		p "Thumbnail not found for #{path} (looked in #{thumbnail_directory_paths.join(', ')} with hash #{file_name})"
 	end
 
 private
 
-	def thumbnail_directory_path
-		#File.join(Dir.home, '.thumbnails/normal')
-		File.join(Dir.home, '.cache/thumbnails/normal')		# new as of ubuntu 15.04
+	def thumbnail_directory_paths
+		[
+			File.join(Dir.home, '.thumbnails/normal'),
+			File.join(Dir.home, '.cache/thumbnails/normal'),		# new as of ubuntu 15.04
+		]
 	end
 
 	def thumbnail_file_name_for_path(path)
