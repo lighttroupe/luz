def0da1b9eddbbe613c4761b8b7f2fcee9c1e25a
Ian McIntosh <ian@openanswers.org>
2017-12-19 03:35:59 -0800

    - numeric input UX: ignore empty, up/down keys act like scrollwheel, focus on single click, show current value until typing

--- gui/gui_numeric.rb
+++ gui/gui_numeric.rb
@@ -3,12 +3,14 @@ class GuiNumeric < GuiValue
 
 	pipe [:text_align=, :width=], :value_label
 
+	EDIT_MODE_COLOR = [1.0,1.0,0.0]
+
 	def initialize(object, method, min, max)
 		super(object, method)
 		@min, @max = min, max
 		@value_label = GuiLabel.new.set(:width => 4, :text_align => :right)		#.set(:scale_x => 0.9, :scale_y => 0.65, :offset_y => -0.12)
 		@value_change_in_progress = ''
-		@gui_string = GuiString.new(self, :value_change_in_progress).set(:color => [1.0,1.0,0.0], :width => 4, :text_align => :center)
+		@gui_string = GuiString.new(self, :value_change_in_progress).set(:color => EDIT_MODE_COLOR, :width => 4, :text_align => :center)
 		@color = [0.8, 0.8, 1.0, 1.0]
 	end
 
@@ -37,9 +39,9 @@ class GuiNumeric < GuiValue
 	#
 	def gui_render
 		with_gui_object_properties {
-			if keyboard_focus?
+			if keyboard_focus? && @value_change_in_progress.length > 0
 				# faint display of current value
-				with_alpha(0.15) {
+				with_alpha(0.1) {
 					@value_label.set_string(generate_string)		# TODO: avoid this work
 					@value_label.gui_render
 				}
@@ -47,10 +49,15 @@ class GuiNumeric < GuiValue
 				@gui_string.gui_render
 
 				# outline for edit mode
-				with_color([0.3,0.3,0.0]) { unit_square_outline }
+				with_color(EDIT_MODE_COLOR) { unit_square_outline }
 			else
 				@value_label.set_string(generate_string)		# TODO: avoid this work
-				@value_label.gui_render
+
+				if keyboard_focus?
+					with_color(EDIT_MODE_COLOR) { @value_label.gui_render }
+				else
+					@value_label.gui_render
+				end
 			end
 		}
 	end
@@ -66,20 +73,35 @@ class GuiNumeric < GuiValue
 		set_value(purify_value(calculate_step_value(:down)))
 	end
 
-	# double-clicking numberic fields begins keyboard edit mode
 	def double_click(pointer)
-		grab_keyboard_focus!
 	end
 
 	def click(pointer)
+		grab_keyboard_focus!
 	end
+
+	NUMERIC_CHARACTERS = ['0','1','2','3','4','5','6','7','8','9','-','.']
 
 	def on_key_press(key)
 		case key
+		when 'space'
+			# ignore
+		when 'escape'
+			@gui_string.set_value('')
+			cancel_keyboard_focus!
+		when 'backspace'
+			@gui_string.on_key_press(key)
+		when 'up'
+			scroll_up!(pointer=nil)
+		when 'down'
+			scroll_down!(pointer=nil)
 		when 'return'
+			cancel_keyboard_focus!
+
 			# finalize manual value change
 			# feature: hold shift to animate the value to the new setting
-			value = clamp_value(purify_value(@value_change_in_progress.to_f))
+			value = current_user_input
+			return unless value
 			if key.shift?
 				add_animation(:value, value, duration=$settings['value-animation-time'].to_f)
 			else
@@ -87,13 +109,16 @@ class GuiNumeric < GuiValue
 				set_value(value)
 			end
 			@gui_string.set_value('')
-			cancel_keyboard_focus!
-		when 'escape'
-			@gui_string.set_value('')
-			cancel_keyboard_focus!
+
+			#grab_keyboard_focus!
 		else
-			@gui_string.on_key_press(key)
+			@gui_string.on_key_press(key) if NUMERIC_CHARACTERS.include?(key)
 		end
+	end
+
+	def current_user_input
+		return nil unless @value_change_in_progress.strip.length > 0
+		clamp_value(purify_value(@value_change_in_progress.to_f))
 	end
 
 	#
