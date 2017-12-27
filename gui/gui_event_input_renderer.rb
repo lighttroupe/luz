3782becedf57a607c0f435e2aaa4d8bec1f3737a
Ian McIntosh <ian@openanswers.org>
2017-12-27 01:46:50 -0800

    - event input background lights up like event

--- gui/gui_event_input_renderer.rb
+++ gui/gui_event_input_renderer.rb
@@ -1,31 +1,22 @@
 class	GuiEventInputRenderer < GuiChildUserObjectRenderer
-	GUI_COLOR_ON = [1.0,1.0,0.0,1.0]
-	GUI_COLOR_OFF = [1.0,1.0,0.0,0.1]
+	BACKGROUND_COLOR_ON = [1.0,1.0,0.0,0.2]
+	BACKGROUND_COLOR_ON_HOVERING = [1.0,1.0,0.0,0.4]
 
 	def gui_render
 		gui_render_background
 		gui_render_label
-		gui_render_on_off_state
 		gui_render_enable_checkbox
 	end
 
-private
-
-	def gui_render_label
-		with_translation(-0.05, 0.0) {
-			with_scale(0.9, 1.0) {
-				super
-			}
-		}
-	end
-
-	def gui_render_on_off_state
-		with_translation(0.5 - 0.05, 0.0) {
-			with_scale(0.05, 0.35) {
-				with_color(@object.now? ? GUI_COLOR_ON : GUI_COLOR_OFF) {
-					unit_square
-				}
-			}
-		}
+	def background_color
+		if @object.now?
+			if pointer_hovering? || selected?
+				BACKGROUND_COLOR_ON_HOVERING
+			else
+				BACKGROUND_COLOR_ON
+			end
+		else
+			super
+		end
 	end
 end
