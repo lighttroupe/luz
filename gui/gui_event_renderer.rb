f770e655fe18f834c61c98d5d55295ae3cce9ce3
Ian McIntosh <ian@openanswers.org>
2017-12-26 22:53:12 -0800

    - event background lights up instead of little square

--- gui/gui_event_renderer.rb
+++ gui/gui_event_renderer.rb
@@ -1,34 +1,23 @@
 class	GuiEventRenderer < GuiUserObjectRenderer
-	GUI_COLOR_ON = [1.0,1.0,0.0,1.0]
-	GUI_COLOR_OFF = [1.0,1.0,0.0,0.1]
+	COLOR_ON = [0.3,0.3,0.0,1.0]
+	COLOR_OFF = [1.0,1.0,0.0,0.0]
 
 	def gui_render
 		gui_render_background
 		gui_render_label
-		gui_render_on_off_state
+	end
+
+	def background_color
+		if @object.now?
+			COLOR_ON
+		else
+			COLOR_OFF
+		end
 	end
 
 private
 
 	def label_width
 		9
-	end
-
-	def gui_render_on_off_state
-		with_translation(0.5 - 0.08, 0.0) {
-			with_scale(0.1, 0.35) {
-				with_color(@object.now? ? GUI_COLOR_ON : GUI_COLOR_OFF) {
-					unit_square
-				}
-			}
-		}
-	end
-
-	def gui_render_label
-		with_translation(-0.08, 0.0) {
-			with_scale(0.75, 1.0) {
-				super
-			}
-		}
 	end
 end
