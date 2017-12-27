cd5c4c73a35ae91f4122c01082a33bbf13c054b5
Ian McIntosh <ian@openanswers.org>
2017-12-26 18:38:29 -0800

    - code cleanup

--- gui/gui_window.rb
+++ gui/gui_window.rb
@@ -2,22 +2,20 @@ class GuiWindow < GuiBox
 	callback :open
 	callback :close
 
+	def open?
+		in_state?(:open)
+	end
 	def open!
 		switch_state({:closed => :open}, duration=0.3)
 		open_notify
 	end
 
-	def open?
-		in_state?(:open)
+	def closed?
+		in_state?(:closed)
 	end
-
 	def close!
 		switch_state({:open => :closed}, duration=0.1)
 		close_notify
-	end
-
-	def closed?
-		!open?
 	end
 
 	# scroll wheel over a window shouldn't leak to the background
