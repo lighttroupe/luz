361fbe87f33c7c0e41b4ae5daca1cbfb6cb3208d
Ian McIntosh <ian@openanswers.org>
2017-12-28 02:56:29 -0800

    - code cleanup

--- gui/gui_message_bar.rb
+++ gui/gui_message_bar.rb
@@ -9,14 +9,11 @@ class GuiMessageBar < GuiBox
 		super
 		self << (@background = GuiObject.new.set(:background_image => $engine.load_image('images/message-bar-background.png')))
 		self << (@text = GuiLabel.new.set(:width => 20, :text_align => :center))
-	end
-
-	def messages
-		@messages ||= []
+		@messages = []
 	end
 
 	def positive_message(message)
-		messages << message
+		@messages << message unless message.blank?
 	end
 	alias :negative_message :positive_message		# for now
 
@@ -25,21 +22,15 @@ class GuiMessageBar < GuiBox
 		next_message! if next_message?
 	end
 
-	def desired_duration
-		(messages.size > 0) ? MESSAGE_DURATION_FAST : MESSAGE_DURATION
-	end
-
-	def current_duration
-		(@message_time.nil?) ? 0.0 : $env[:frame_time] - @message_time
-	end
+private
 
 	def next_message?
-		(@message_time.nil? && !messages.empty?) ||		# introduce a message?
+		(@message_time.nil? && !@messages.empty?) ||		# introduce a message?
 		(current_duration > desired_duration)		# move to next message?
 	end
 
 	def next_message!
-		message = messages.shift
+		message = @messages.shift
 		if message
 			@text.set_string(message).set({:hidden => false})
 			animate(:opacity, 1.0, duration=0.01)
@@ -48,5 +39,13 @@ class GuiMessageBar < GuiBox
 			animate(:opacity, 0.0, duration=0.2) { }
 			@message_time = nil
 		end
+	end
+
+	def desired_duration
+		(@messages.size > 0) ? MESSAGE_DURATION_FAST : MESSAGE_DURATION
+	end
+
+	def current_duration
+		(@message_time.nil?) ? 0.0 : $env[:frame_time] - @message_time
 	end
 end
