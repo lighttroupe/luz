f76619dcf199c6625af87398939a75a388b5ecc4
Ian McIntosh <ian@openanswers.org>
2018-01-16 00:57:40 -0800

    - UOSSelect fewer shows characters, wider

--- gui/addons/user_object_setting_select.rb
+++ gui/addons/user_object_setting_select.rb
@@ -2,7 +2,7 @@ class UserObjectSettingSelect
 	def gui_build_editor
 		box = GuiBox.new
 		box << create_user_object_setting_name_label
-		box << GuiSelect.new(self, :selected, @options[:options]).set(:width => 20, :scale_x => 0.5, :scale_y => 0.45, :float => :left, :offset_y => 0.1)
+		box << GuiSelect.new(self, :selected, @options[:options]).set(:width => 12, :scale_x => 0.5, :scale_y => 0.45, :float => :left, :offset_y => 0.1)
 		box
 	end
 end
