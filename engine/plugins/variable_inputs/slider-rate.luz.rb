96e5bae137e801dc8e13fd0fa49b151bd1c78609
Ian McIntosh <ian@openanswers.org>
2018-10-28 01:07:27 -0700

    - add empty_time setting and fix slider value to change rate calculation

--- engine/plugins/variable_inputs/slider-rate.luz.rb
+++ engine/plugins/variable_inputs/slider-rate.luz.rb
@@ -7,12 +7,13 @@ class VariableInputSliderRate < VariableInput
 	setting 'slider', :slider, :summary => true
 	setting 'pivot_point', :float, :range => 0.001..0.999, :default => 0.5..0.999
 	setting 'fill_time', :timespan, :summary => true
+	setting 'empty_time', :timespan, :summary => true
 
 	def value
 		if slider > pivot_point
-			(last_value + (($env[:frame_time_delta] / fill_time.to_seconds) * ((slider - pivot_point) / pivot_point)))
+			(last_value + (($env[:frame_time_delta] / fill_time.to_seconds) * ((slider - pivot_point) / (1.0 - pivot_point))))
 		else
-			(last_value - (($env[:frame_time_delta] / fill_time.to_seconds) * ((pivot_point - slider) / (1.0 - pivot_point))))
+			(last_value - (($env[:frame_time_delta] / empty_time.to_seconds) * ((pivot_point - slider) / pivot_point)))
 		end
 	end
 end
